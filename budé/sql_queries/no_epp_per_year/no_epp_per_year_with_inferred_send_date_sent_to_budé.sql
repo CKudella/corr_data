7493c03588fdb5f82da48e848e6c0f7f21ef24ce
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-03 12:49:26 +0100

    Added six SQL scripts; Added Readme

---
+++ budé/sql_queries/no_epp_per_year/no_epp_per_year_with_inferred_send_date_sent_to_budé.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT send_date_year1 AS 'Year', COUNT(letters_id) AS 'Number of letters with inferred send date sent to Budé this year' FROM letters WHERE letters_id NOT LIKE '%ck2%' AND send_date_inferred = '1' AND recipient_id = 'budé_guillaume_viaf_105878228' GROUP BY send_date_year1
