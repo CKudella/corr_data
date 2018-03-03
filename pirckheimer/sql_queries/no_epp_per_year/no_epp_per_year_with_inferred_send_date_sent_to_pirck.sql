9974b7dd533d6d8ac1a91d4d31b0db68522aa4e9
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-03 16:43:40 +0100

    Added six SQL scripts + Readme to Pirckheimer no_epp_per_year

---
+++ pirckheimer/sql_queries/no_epp_per_year/no_epp_per_year_with_inferred_send_date_sent_to_pirck.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT send_date_year1 AS 'Year', COUNT(letters_id) AS 'No of Letters with inferred Date of Dispatch sent TO Pirckheimer' FROM letters WHERE letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]' AND send_date_inferred = '1' AND recipient_id = 'pirckheimer_willibald_viaf_27173507' GROUP BY send_date_year1
