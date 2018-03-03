7ce1b58d96f50fcfbbb919e30a7e34da062659a0
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-03 11:18:07 +0100

    Cleanup Budé corpus_overview; Added two scripts and Readme file

---
+++ budé/sql_queries/corpus_overview/no_locs_corr_receives_epp_at.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT recipient_id, COUNT(DISTINCT target_loc_id) AS 'Number of locations this correspondent receives letters at' FROM letters GROUP BY recipient_id ORDER BY recipient_id
--- budé/sql_queries/corpus_overview/no_of_correspondents_only_individuals.sql
+++ budé/sql_queries/corpus_overview/no_of_correspondents_only_individuals.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(*) AS 'Total Number of indiviudal Correspondents of Budé' FROM correspondents WHERE correspondents_id NOT IN ('budé_guillaume_viaf_105878228','unnamed_person_viaf_unknown') AND type = 'indiviudal'
+SELECT COUNT(*) AS 'Total number of correspondents (type: individual) of Budé' FROM correspondents WHERE correspondents_id NOT IN ('budé_guillaume_viaf_105878228','unnamed_person_viaf_unknown') AND type = 'individual'
