caeef8b91c19086148a936daa3c898a3456515a7
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-03 17:00:18 +0100

    Added Pirckheimer duration_of_correspondence scripts

---
+++ pirckheimer/sql_queries/duration_of_correspondence/dates_first_and_last_epp_to_pirck_from_corr.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT sender_id, MIN(send_date_computable1) AS 'First Letter to Pirckheimer', MAX(send_date_computable1) AS 'Last Letter to Pirckheimer' FROM letters WHERE recipient_id = 'pirckheimer_willibald_viaf_27173507' GROUP BY sender_id
