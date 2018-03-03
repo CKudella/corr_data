9827dcc582aa0ec16ce71ebcae44667e7a725fff
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-03 12:33:14 +0100

    Added four SQL scripts to Budé no_epp_per_loc; Added the respective Readme

---
+++ budé/sql_queries/no_epp_per_loc/comp_epp_per_loc_written_by_budé_at_and_sent_by_budé_to.sql
@@ -0,0 +1,7 @@
+SELECT X.LocationName, X.Latitude, X.Longitude, X.NoLettersTOBudé, Y.NoLettersBYBudé FROM
+(
+SELECT XB.locations_name_modern AS LocationName, XB.locations_lat AS 'Latitude', XB.locations_lng as 'Longitude', COUNT(XA.source_loc_id) AS NoLettersTOBudé FROM letters AS XA JOIN locations AS XB ON XB.locations_id = XA.source_loc_id WHERE XA.letters_id NOT LIKE '%ck2' AND XA.recipient_id = 'budé_guillaume_viaf_105878228' GROUP BY XA.source_loc_id ORDER BY COUNT(XA.source_loc_id) DESC
+) AS X INNER JOIN
+(
+SELECT YB.locations_name_modern AS LocationName, YB.locations_lat AS 'Latitude', YB.locations_lng as 'Longitude', COUNT(YA.source_loc_id) AS NoLettersBYBudé FROM letters AS YA JOIN locations AS YB ON YB.locations_id = YA.source_loc_id WHERE YA.letters_id NOT LIKE '%ck2' AND YA.sender_id = 'budé_guillaume_viaf_105878228' GROUP BY YA.source_loc_id ORDER BY COUNT(YA.source_loc_id) DESC
+) AS Y ON Y.LocationName = X.LocationName
