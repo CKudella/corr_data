7ff3e9e98007749865a4fb8a1614e7d8dd49884a
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-04 12:59:57 +0100

    Incorrect queries temporarily removed

--- pirckheimer/sql_queries/no_epp_per_modern_state_year/no_epp_from_to_pirck_modern_state_per_year.sql
+++
@@ -1,5 +0,0 @@
-SELECT X.ModernState, X.COUNT AS 'Number of letters Pirckeimer sent to this modern state this year', Y.COUNT AS 'Number of letters sent from this modern state this year to Pirckheimer', X.send_date_year1 FROM
-(SELECT DISTINCT locations_modern_state AS 'ModernState', COUNT(*) AS COUNT, send_date_year1 FROM letters, locations WHERE locations_id = target_loc_id AND letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]' AND sender_id = 'pirckheimer_willibald_viaf_27173507' GROUP BY locations_modern_state, send_date_year1 ORDER BY send_date_year1 ASC) AS X
-INNER JOIN
-(SELECT DISTINCT locations_modern_state AS 'ModernState', COUNT(*) AS COUNT, send_date_year1 FROM letters, locations WHERE locations_id = source_loc_id AND letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]' AND recipient_id = 'pirckheimer_willibald_viaf_27173507' GROUP BY locations_modern_state, send_date_year1 ORDER BY send_date_year1 ASC) AS Y
-ON X.ModernState = Y.ModernState AND X.send_date_year1 = Y.send_date_year1 ORDER BY X.send_date_year1
