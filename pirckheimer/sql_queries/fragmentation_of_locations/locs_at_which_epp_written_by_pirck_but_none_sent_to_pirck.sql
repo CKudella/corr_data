fcae844c6466c6a0792e18562c8ad570480d271e
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 16:36:12 +0100

    Beautified code

--- pirckheimer/sql_queries/fragmentation_of_locations/locs_at_which_epp_written_by_pirck_but_none_sent_to_pirck.sql
+++ pirckheimer/sql_queries/fragmentation_of_locations/locs_at_which_epp_written_by_pirck_but_none_sent_to_pirck.sql
@@ -1,1 +1,14 @@
-SELECT locations.locations_name_modern FROM locations WHERE locations.locations_id IN (SELECT DISTINCT source_loc_id FROM letters WHERE sender_id = 'pirckheimer_willibald_viaf_27173507' AND source_loc_id NOT LIKE 'unknown%' GROUP BY source_loc_id) AND locations.locations_id NOT IN (SELECT DISTINCT source_loc_id FROM letters WHERE recipient_id = 'pirckheimer_willibald_viaf_27173507' AND source_loc_id NOT LIKE 'unknown%' GROUP BY source_loc_id)
+SELECT locations.locations_name_modern
+FROM locations
+WHERE locations.locations_id IN
+    (SELECT DISTINCT source_loc_id
+     FROM letters
+     WHERE sender_id = 'pirckheimer_willibald_viaf_27173507'
+       AND source_loc_id NOT LIKE 'unknown%'
+     GROUP BY source_loc_id)
+  AND locations.locations_id NOT IN
+    (SELECT DISTINCT source_loc_id
+     FROM letters
+     WHERE recipient_id = 'pirckheimer_willibald_viaf_27173507'
+       AND source_loc_id NOT LIKE 'unknown%'
+     GROUP BY source_loc_id)
--- pirckheimer/sql_queries/fragmentation_of_locations/locs_with_epp_sent_to_by_pirck_but_not_vice_versa.sql
+++ pirckheimer/sql_queries/fragmentation_of_locations/locs_with_epp_sent_to_by_pirck_but_not_vice_versa.sql
@@ -1,1 +1,16 @@
-SELECT DISTINCT A.target_loc_id, TargetLoc.locations_name_modern AS TargetNameModern, TargetLoc.locations_lat AS TargetLAT, TargetLoc.locations_lng AS TargetLNG, A.source_loc_id, SourceLoc.locations_name_modern AS SourceNameModern, SourceLoc.locations_lat AS SourceLAT, SourceLoc.locations_lng AS SourceLNG FROM letters as A LEFT JOIN locations AS SourceLoc ON SourceLoc.locations_id = A.source_loc_id LEFT JOIN locations AS TargetLoc ON TargetLoc.locations_id = A.target_loc_id WHERE A.sender_id = 'pirckheimer_willibald_viaf_27173507' AND A.target_loc_id NOT IN (SELECT DISTINCT B.source_loc_id FROM letters AS B WHERE B.sender_id != 'pirckheimer_willibald_viaf_27173507')
+SELECT DISTINCT A.target_loc_id,
+                TargetLoc.locations_name_modern AS TargetNameModern,
+                TargetLoc.locations_lat AS TargetLAT,
+                TargetLoc.locations_lng AS TargetLNG,
+                A.source_loc_id,
+                SourceLoc.locations_name_modern AS SourceNameModern,
+                SourceLoc.locations_lat AS SourceLAT,
+                SourceLoc.locations_lng AS SourceLNG
+FROM letters AS A
+LEFT JOIN locations AS SourceLoc ON SourceLoc.locations_id = A.source_loc_id
+LEFT JOIN locations AS TargetLoc ON TargetLoc.locations_id = A.target_loc_id
+WHERE A.sender_id = 'pirckheimer_willibald_viaf_27173507'
+  AND A.target_loc_id NOT IN
+    (SELECT DISTINCT B.source_loc_id
+     FROM letters AS B
+     WHERE B.sender_id != 'pirckheimer_willibald_viaf_27173507')
