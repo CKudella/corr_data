abe7d3ec2946d8a90c5a752055bc92570ff83617
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 16:42:39 +0100

    Beautified code

--- pirckheimer/sql_queries/no_epp_per_modern_state/no_epp_per_modern_state_sent_from_pirck_to.sql
+++ pirckheimer/sql_queries/no_epp_per_modern_state/no_epp_per_modern_state_sent_from_pirck_to.sql
@@ -1,1 +1,9 @@
-SELECT DISTINCT locations.locations_modern_state AS 'Modern State', COUNT(*) AS 'Number of letters Pirckheimer sent to this modern state' FROM letters, locations WHERE locations.locations_id = letters.target_loc_id AND letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]' AND sender_id = 'pirckheimer_willibald_viaf_27173507' GROUP BY locations_modern_state ORDER BY COUNT(*) DESC
+SELECT DISTINCT locations.locations_modern_state AS 'Modern State',
+                COUNT(*) AS 'Number of letters Pirckheimer sent to this modern state'
+FROM letters,
+     locations
+WHERE locations.locations_id = letters.target_loc_id
+  AND letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+  AND sender_id = 'pirckheimer_willibald_viaf_27173507'
+GROUP BY locations_modern_state
+ORDER BY COUNT(*) DESC
--- pirckheimer/sql_queries/no_epp_per_modern_state/no_epp_per_modern_state_sent_to_pirck.sql
+++ pirckheimer/sql_queries/no_epp_per_modern_state/no_epp_per_modern_state_sent_to_pirck.sql
@@ -1,1 +1,9 @@
-SELECT DISTINCT locations.locations_modern_state AS 'Modern State', COUNT(*) AS 'Number of letters sent from this modern state to Pirckheimer' FROM letters, locations WHERE locations.locations_id = letters.source_loc_id AND letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]' AND recipient_id = 'pirckheimer_willibald_viaf_27173507' GROUP BY locations_modern_state ORDER BY COUNT(*) DESC
+SELECT DISTINCT locations.locations_modern_state AS 'Modern State',
+                COUNT(*) AS 'Number of letters sent from this modern state to Pirckheimer'
+FROM letters,
+     locations
+WHERE locations.locations_id = letters.source_loc_id
+  AND letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+  AND recipient_id = 'pirckheimer_willibald_viaf_27173507'
+GROUP BY locations_modern_state
+ORDER BY COUNT(*) DESC
