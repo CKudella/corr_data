96b0769fea3115ef9df2c6be1a4145466e864497
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 15:19:04 +0100

    Beautified code for enrich_epp_with_geocoordinates SQL scripts

--- erasmus/sql_queries/enrich_epp_with_geocoordinates/all_epp_with_geocoordinates.sql
+++ erasmus/sql_queries/enrich_epp_with_geocoordinates/all_epp_with_geocoordinates.sql
@@ -1,1 +1,13 @@
-SELECT letters_id, sender_id, recipient_id, send_date_computable1, source_loc_id, SourceLoc.locations_lat AS SourceLAT, SourceLoc.locations_lng AS SourceLNG, target_loc_id, TargetLoc.locations_lat AS TargetLAT, TargetLoc.locations_lng AS TargetLNG from era_cdb_v3.letters LEFT JOIN era_cdb_v3.locations AS SourceLoc ON SourceLoc.locations_id = letters.source_loc_id LEFT JOIN era_cdb_v3.locations AS TargetLoc ON TargetLoc.locations_id = letters.target_loc_id
+SELECT letters_id,
+       sender_id,
+       recipient_id,
+       send_date_computable1,
+       source_loc_id,
+       SourceLoc.locations_lat AS SourceLAT,
+       SourceLoc.locations_lng AS SourceLNG,
+       target_loc_id,
+       TargetLoc.locations_lat AS TargetLAT,
+       TargetLoc.locations_lng AS TargetLNG
+FROM era_cdb_v3.letters
+LEFT JOIN era_cdb_v3.locations AS SourceLoc ON SourceLoc.locations_id = letters.source_loc_id
+LEFT JOIN era_cdb_v3.locations AS TargetLoc ON TargetLoc.locations_id = letters.target_loc_id
