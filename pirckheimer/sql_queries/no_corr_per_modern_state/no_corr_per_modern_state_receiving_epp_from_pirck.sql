ca2ef3bf90759bb56407cea5de4c6924e36b858c
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 16:38:43 +0100

    Beautified code

--- pirckheimer/sql_queries/no_corr_per_modern_state/no_corr_per_modern_state_receiving_epp_from_pirck.sql
+++ pirckheimer/sql_queries/no_corr_per_modern_state/no_corr_per_modern_state_receiving_epp_from_pirck.sql
@@ -1,1 +1,8 @@
-SELECT DISTINCT locations.locations_modern_state As 'Modern State', COUNT(DISTINCT recipient_id) AS 'Number of correspondents who received letters from Pirckheimer' FROM letters, locations WHERE locations.locations_id = letters.target_loc_id AND sender_id LIKE 'pirckheimer_willibald_viaf_27173507' GROUP BY locations_modern_state ORDER BY COUNT(DISTINCT recipient_id) DESC
+SELECT DISTINCT locations.locations_modern_state AS 'Modern State',
+                COUNT(DISTINCT recipient_id) AS 'Number of correspondents who received letters from Pirckheimer'
+FROM letters,
+     locations
+WHERE locations.locations_id = letters.target_loc_id
+  AND sender_id LIKE 'pirckheimer_willibald_viaf_27173507'
+GROUP BY locations_modern_state
+ORDER BY COUNT(DISTINCT recipient_id) DESC
