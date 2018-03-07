b17d9014d4d116ab7591d0b7ca0b3198b786b187
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 16:01:02 +0100

    Beautified code for duration_of_correspondence SQL scripts

--- budé/sql_queries/duration_of_correspondence/dates_first_and_last_epp_from_budé_to_corr.sql
+++ budé/sql_queries/duration_of_correspondence/dates_first_and_last_epp_from_budé_to_corr.sql
@@ -1,1 +1,6 @@
-SELECT DISTINCT recipient_id, MIN(send_date_computable1) AS 'First Letter From Budé', MAX(send_date_computable1) AS 'Last Letter From Budé' FROM letters WHERE sender_id = 'budé_guillaume_viaf_105878228' GROUP BY recipient_id
+SELECT DISTINCT recipient_id,
+                MIN(send_date_computable1) AS 'First Letter From Budé',
+                MAX(send_date_computable1) AS 'Last Letter From Budé'
+FROM letters
+WHERE sender_id = 'budé_guillaume_viaf_105878228'
+GROUP BY recipient_id
