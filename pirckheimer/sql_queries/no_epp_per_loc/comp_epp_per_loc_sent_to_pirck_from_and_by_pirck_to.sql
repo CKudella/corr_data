f969e4b175b8254e0500a4d45e2dd13502665e81
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 16:42:00 +0100

    Beautified code

--- pirckheimer/sql_queries/no_epp_per_loc/comp_epp_per_loc_sent_to_pirck_from_and_by_pirck_to.sql
+++ pirckheimer/sql_queries/no_epp_per_loc/comp_epp_per_loc_sent_to_pirck_from_and_by_pirck_to.sql
@@ -1,7 +1,27 @@
-SELECT X.LocationName, X.Latitude, X.Longitude, X.NoLettersWrittenTOPirckheimer, Y.NoLettersWrittenBYPirckheimerTO FROM
-(
-SELECT XB.locations_name_modern AS LocationName, XB.locations_lat AS 'Latitude', XB.locations_lng as 'Longitude', COUNT(XA.source_loc_id) AS NoLettersWrittenTOPirckheimer FROM letters AS XA JOIN locations AS XB ON XB.locations_id = XA.source_loc_id WHERE XA.letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]' AND XA.recipient_id = 'pirckheimer_willibald_viaf_27173507' GROUP BY XA.source_loc_id ORDER BY COUNT(XA.source_loc_id) DESC
-) AS X INNER JOIN
-(
-SELECT YB.locations_name_modern AS LocationName, YB.locations_lat AS 'Latitude', YB.locations_lng as 'Longitude', COUNT(YA.target_loc_id) AS NoLettersWrittenBYPirckheimerTO FROM letters AS YA JOIN locations AS YB ON YB.locations_id = YA.target_loc_id WHERE YA.letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]' AND YA.recipient_id != 'pirckheimer_willibald_viaf_27173507' GROUP BY YA.target_loc_id ORDER BY COUNT(YA.target_loc_id) DESC
-) AS Y ON Y.LocationName = X.LocationName
+SELECT X.LocationName,
+       X.Latitude,
+       X.Longitude,
+       X.NoLettersWrittenTOPirckheimer,
+       Y.NoLettersWrittenBYPirckheimerTO
+FROM
+  (SELECT XB.locations_name_modern AS LocationName,
+          XB.locations_lat AS 'Latitude',
+          XB.locations_lng AS 'Longitude',
+          COUNT(XA.source_loc_id) AS NoLettersWrittenTOPirckheimer
+   FROM letters AS XA
+   JOIN locations AS XB ON XB.locations_id = XA.source_loc_id
+   WHERE XA.letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+     AND XA.recipient_id = 'pirckheimer_willibald_viaf_27173507'
+   GROUP BY XA.source_loc_id
+   ORDER BY COUNT(XA.source_loc_id) DESC) AS X
+INNER JOIN
+  (SELECT YB.locations_name_modern AS LocationName,
+          YB.locations_lat AS 'Latitude',
+          YB.locations_lng AS 'Longitude',
+          COUNT(YA.target_loc_id) AS NoLettersWrittenBYPirckheimerTO
+   FROM letters AS YA
+   JOIN locations AS YB ON YB.locations_id = YA.target_loc_id
+   WHERE YA.letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+     AND YA.recipient_id != 'pirckheimer_willibald_viaf_27173507'
+   GROUP BY YA.target_loc_id
+   ORDER BY COUNT(YA.target_loc_id) DESC) AS Y ON Y.LocationName = X.LocationName
