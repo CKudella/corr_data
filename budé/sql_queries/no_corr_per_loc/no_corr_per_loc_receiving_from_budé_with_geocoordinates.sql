16b72da898a30a08c2da7e193afe1a374e0860a4
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 16:05:37 +0100

    Beautified code for no_corr_per_loc SQL scripts

--- budé/sql_queries/no_corr_per_loc/no_corr_per_loc_receiving_from_budé_with_geocoordinates.sql
+++ budé/sql_queries/no_corr_per_loc/no_corr_per_loc_receiving_from_budé_with_geocoordinates.sql
@@ -1,1 +1,9 @@
-SELECT locations_name_modern AS 'Location Name Modern', locations_lat AS Latitude, locations_lng AS Longitude, COUNT(DISTINCT recipient_id) AS 'Number of correspondents who received at this Location letters from Budé' FROM letters JOIN budé_cdb_v1.locations ON locations.locations_id = letters.target_loc_id WHERE sender_id = 'budé_guillaume_viaf_105878228' GROUP BY target_loc_id ORDER BY COUNT(DISTINCT recipient_id) DESC
+SELECT locations_name_modern AS 'Location Name Modern',
+       locations_lat AS Latitude,
+       locations_lng AS Longitude,
+       COUNT(DISTINCT recipient_id) AS 'Number of correspondents who received at this location letters from Budé'
+FROM letters
+JOIN budé_cdb_v1.locations ON locations.locations_id = letters.target_loc_id
+WHERE sender_id = 'budé_guillaume_viaf_105878228'
+GROUP BY target_loc_id
+ORDER BY COUNT(DISTINCT recipient_id) DESC
