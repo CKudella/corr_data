f15236ef5f89b5940987b03f6c18a6d803116edc
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 16:35:20 +0100

    Beautified code

--- pirckheimer/sql_queries/fragmentation_of_correspondents/names_of_corr_with_epp_from_and_to_pirck.sql
+++ pirckheimer/sql_queries/fragmentation_of_correspondents/names_of_corr_with_epp_from_and_to_pirck.sql
@@ -1,1 +1,19 @@
-SELECT name_in_edition FROM correspondents WHERE name_in_edition NOT IN (SELECT name_in_edition AS 'Correspondents who wrote to but did not receive any letters by Pirckheimer' FROM correspondents WHERE correspondents.correspondents_id NOT IN (SELECT DISTINCT recipient_id FROM letters WHERE recipient_id != 'pirckheimer_willibald_viaf_27173507') GROUP BY name_in_edition) AND name_in_edition NOT IN (SELECT name_in_edition FROM correspondents, letters WHERE correspondents.correspondents_id NOT IN (SELECT DISTINCT sender_id FROM letters WHERE sender_id != 'pirckheimer_willibald_viaf_27173507') GROUP BY name_in_edition)
+SELECT name_in_edition
+FROM correspondents
+WHERE name_in_edition NOT IN
+    (SELECT name_in_edition AS 'Correspondents who wrote to but did not receive any letters by Pirckheimer'
+     FROM correspondents
+     WHERE correspondents.correspondents_id NOT IN
+         (SELECT DISTINCT recipient_id
+          FROM letters
+          WHERE recipient_id != 'pirckheimer_willibald_viaf_27173507')
+     GROUP BY name_in_edition)
+  AND name_in_edition NOT IN
+    (SELECT name_in_edition
+     FROM correspondents,
+          letters
+     WHERE correspondents.correspondents_id NOT IN
+         (SELECT DISTINCT sender_id
+          FROM letters
+          WHERE sender_id != 'pirckheimer_willibald_viaf_27173507')
+     GROUP BY name_in_edition)
--- pirckheimer/sql_queries/fragmentation_of_correspondents/names_of_corr_with_only_epp_to_pirck.sql
+++ pirckheimer/sql_queries/fragmentation_of_correspondents/names_of_corr_with_only_epp_to_pirck.sql
@@ -1,1 +1,6 @@
-SELECT name_in_edition AS 'Correspondents who wrote to but did not receive any letters by Pirckheimer' FROM correspondents WHERE correspondents.correspondents_id NOT IN (SELECT DISTINCT recipient_id FROM letters) GROUP BY name_in_edition
+SELECT name_in_edition AS 'Correspondents who wrote to but did not receive any letters by Pirckheimer'
+FROM correspondents
+WHERE correspondents.correspondents_id NOT IN
+    (SELECT DISTINCT recipient_id
+     FROM letters)
+GROUP BY name_in_edition
