c1c92dd5572fae06e4f2a341ab58cf106a6c5fc7
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 15:36:59 +0100

    Beautified code for no_epp_per_correspondent SQL scripts

--- erasmus/sql_queries/no_epp_per_correspondent/no_epp_per_cor_written_by_era.sql
+++ erasmus/sql_queries/no_epp_per_correspondent/no_epp_per_cor_written_by_era.sql
@@ -1,1 +1,6 @@
-SELECT DISTINCT recipient_id, COUNT(*) 'Number of letters sent from Erasmus to this correspondent' from letters WHERE sender_id = 'erasmus_desiderius_viaf_95982394' GROUP BY recipient_id ORDER BY COUNT(*) DESC
+SELECT DISTINCT recipient_id,
+                COUNT(*) 'Number of letters sent from Erasmus to this correspondent'
+FROM letters
+WHERE sender_id = 'erasmus_desiderius_viaf_95982394'
+GROUP BY recipient_id
+ORDER BY COUNT(*) DESC
