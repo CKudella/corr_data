0aa97bcb3bf69dfbfa2175a26951708a48455548
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 15:31:28 +0100

    Beautified code for fragmentation_of_locations SQL scripts

--- erasmus/sql_queries/fragmentation_of_locations/locs_at_which_epp_written_by_era_and_sent_to_era.sql
+++ erasmus/sql_queries/fragmentation_of_locations/locs_at_which_epp_written_by_era_and_sent_to_era.sql
@@ -1,1 +1,14 @@
-SELECT locations.locations_name_modern FROM locations WHERE locations.locations_id IN (SELECT DISTINCT source_loc_id FROM letters WHERE sender_id = 'erasmus_desiderius_viaf_95982394' AND source_loc_id NOT LIKE 'unknown%' GROUP BY source_loc_id) AND locations.locations_id IN (SELECT DISTINCT source_loc_id FROM letters WHERE recipient_id = 'erasmus_desiderius_viaf_95982394' AND source_loc_id NOT LIKE 'unknown%' GROUP BY source_loc_id)
+SELECT locations.locations_name_modern
+FROM locations
+WHERE locations.locations_id IN
+    (SELECT DISTINCT source_loc_id
+     FROM letters
+     WHERE sender_id = 'erasmus_desiderius_viaf_95982394'
+       AND source_loc_id NOT LIKE 'unknown%'
+     GROUP BY source_loc_id)
+  AND locations.locations_id IN
+    (SELECT DISTINCT source_loc_id
+     FROM letters
+     WHERE recipient_id = 'erasmus_desiderius_viaf_95982394'
+       AND source_loc_id NOT LIKE 'unknown%'
+     GROUP BY source_loc_id)
