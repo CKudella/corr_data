39d6f9cb675d5bff318ba1f58f32d0b8fe9e68ed
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 15:30:02 +0100

    Beautified code for fragmentation_of_correspondents SQL scripts

--- erasmus/sql_queries/fragmentation_of_correspondents/names_of_corr_with_only_epp_by_era.sql
+++ erasmus/sql_queries/fragmentation_of_correspondents/names_of_corr_with_only_epp_by_era.sql
@@ -1,1 +1,6 @@
-SELECT name_in_edition AS 'Names of correspondents who received letters from Erasmus but did not write to him' FROM correspondents WHERE correspondents.correspondents_id NOT IN (SELECT DISTINCT sender_id FROM letters) GROUP BY name_in_edition
+SELECT name_in_edition AS 'Names of correspondents who received letters from Erasmus but did not write to him'
+FROM correspondents
+WHERE correspondents.correspondents_id NOT IN
+    (SELECT DISTINCT sender_id
+     FROM letters)
+GROUP BY name_in_edition
--- erasmus/sql_queries/fragmentation_of_correspondents/names_of_corr_with_only_epp_to_era.sql
+++ erasmus/sql_queries/fragmentation_of_correspondents/names_of_corr_with_only_epp_to_era.sql
@@ -1,1 +1,6 @@
-SELECT name_in_edition AS 'Names of correspondents who wrote letters to Erasmus but did not receive any letters from him' FROM correspondents WHERE correspondents.correspondents_id NOT IN (SELECT DISTINCT recipient_id FROM letters) GROUP BY name_in_edition
+SELECT name_in_edition AS 'Names of correspondents who wrote letters to Erasmus but did not receive any letters from him'
+FROM correspondents
+WHERE correspondents.correspondents_id NOT IN
+    (SELECT DISTINCT recipient_id
+     FROM letters)
+GROUP BY name_in_edition
