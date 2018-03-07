2f6886aa446133a87addc57522ed287692aa59fe
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 16:44:50 +0100

    Beautified code

--- pirckheimer/sql_queries/no_epp_per_year/no_epp_per_year_with_inferred_send_date_sent_to_pirck.sql
+++ pirckheimer/sql_queries/no_epp_per_year/no_epp_per_year_with_inferred_send_date_sent_to_pirck.sql
@@ -1,1 +1,7 @@
-SELECT DISTINCT send_date_year1 AS 'Year', COUNT(letters_id) AS 'Number of letters with inferred send date sent to Pirckheimer' FROM letters WHERE letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]' AND send_date_inferred = '1' AND recipient_id = 'pirckheimer_willibald_viaf_27173507' GROUP BY send_date_year1
+SELECT DISTINCT send_date_year1 AS 'Year',
+                COUNT(letters_id) AS 'Number of letters with inferred send date sent to Pirckheimer'
+FROM letters
+WHERE letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+  AND send_date_inferred = '1'
+  AND recipient_id = 'pirckheimer_willibald_viaf_27173507'
+GROUP BY send_date_year1
