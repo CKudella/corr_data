010c8960bd5f820b95ace68624f7cd9b003fa032
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 15:41:43 +0100

    Beautified code forno_epp_per_year SQL scripts

--- erasmus/sql_queries/no_epp_per_year/no_epp_per_year_send_date_year1.sql
+++ erasmus/sql_queries/no_epp_per_year/no_epp_per_year_send_date_year1.sql
@@ -1,1 +1,5 @@
-SELECT send_date_year1, COUNT(*) AS 'Total number of letters sent this year' from letters WHERE letters_id NOT LIKE '%ck2' GROUP BY send_date_year1
+SELECT send_date_year1,
+       COUNT(*) AS 'Total number of letters sent this year'
+FROM letters
+WHERE letters_id NOT LIKE '%ck2'
+GROUP BY send_date_year1
--- erasmus/sql_queries/no_epp_per_year/no_epp_per_year_sent_by_era_send_date_year1.sql
+++ erasmus/sql_queries/no_epp_per_year/no_epp_per_year_sent_by_era_send_date_year1.sql
@@ -1,1 +1,6 @@
-SELECT send_date_year1, COUNT(*) AS 'Total number of letters sent by Erasmus this year' from letters WHERE letters_id NOT LIKE '%ck2' AND sender_id = 'erasmus_desiderius_viaf_95982394' GROUP BY send_date_year1
+SELECT send_date_year1,
+       COUNT(*) AS 'Total number of letters sent by Erasmus this year'
+FROM letters
+WHERE letters_id NOT LIKE '%ck2'
+  AND sender_id = 'erasmus_desiderius_viaf_95982394'
+GROUP BY send_date_year1
