0bdcb4a2cc0247cd711fe37c9cf8bbb078815112
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 16:21:08 +0100

    Beautified code for no_epp_per_year SQL scripts

--- budé/sql_queries/no_epp_per_year/no_epp_per_year_send_date_year1.sql
+++ budé/sql_queries/no_epp_per_year/no_epp_per_year_send_date_year1.sql
@@ -1,1 +1,5 @@
-SELECT send_date_year1, COUNT(*) AS 'Total Number of letters' from letters WHERE letters_id NOT LIKE '%ck2' GROUP BY send_date_year1
+SELECT send_date_year1,
+       COUNT(*) AS 'Total number of letters'
+FROM letters
+WHERE letters_id NOT LIKE '%ck2'
+GROUP BY send_date_year1
--- budé/sql_queries/no_epp_per_year/no_epp_per_year_with_non_inferred_send_date_sent_by_budé.sql
+++ budé/sql_queries/no_epp_per_year/no_epp_per_year_with_non_inferred_send_date_sent_by_budé.sql
@@ -1,1 +1,7 @@
-SELECT DISTINCT send_date_year1 AS 'Year', COUNT(letters_id) AS 'Number of letters with non-inferred send date sent by Budé this year' FROM letters WHERE letters_id NOT LIKE '%ck2%' AND send_date_inferred = '0' AND sender_id = 'budé_guillaume_viaf_105878228' GROUP BY send_date_year1
+SELECT DISTINCT send_date_year1 AS 'Year',
+                COUNT(letters_id) AS 'Number of letters with non-inferred send date sent by Budé this year'
+FROM letters
+WHERE letters_id NOT LIKE '%ck2%'
+  AND send_date_inferred = '0'
+  AND sender_id = 'budé_guillaume_viaf_105878228'
+GROUP BY send_date_year1
