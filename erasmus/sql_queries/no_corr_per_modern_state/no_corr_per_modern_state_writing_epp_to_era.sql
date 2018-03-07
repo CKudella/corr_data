aa0a764152c40164edee883aea8490c72e321c3a
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 15:35:07 +0100

    Beautified code for no_corr_per_modern_state SQL scripts

--- erasmus/sql_queries/no_corr_per_modern_state/no_corr_per_modern_state_writing_epp_to_era.sql
+++ erasmus/sql_queries/no_corr_per_modern_state/no_corr_per_modern_state_writing_epp_to_era.sql
@@ -1,1 +1,8 @@
-SELECT DISTINCT locations.locations_modern_state As 'Modern State', COUNT(DISTINCT sender_id) AS 'Number of correspondents who wrote letters to Erasmus' FROM letters, locations WHERE locations.locations_id = letters.source_loc_id AND recipient_id LIKE 'erasmus_desiderius_viaf_95982394' GROUP BY locations_modern_state ORDER BY COUNT(DISTINCT sender_id) DESC
+SELECT DISTINCT locations.locations_modern_state AS 'Modern State',
+                COUNT(DISTINCT sender_id) AS 'Number of correspondents who wrote letters to Erasmus'
+FROM letters,
+     locations
+WHERE locations.locations_id = letters.source_loc_id
+  AND recipient_id LIKE 'erasmus_desiderius_viaf_95982394'
+GROUP BY locations_modern_state
+ORDER BY COUNT(DISTINCT sender_id) DESC
