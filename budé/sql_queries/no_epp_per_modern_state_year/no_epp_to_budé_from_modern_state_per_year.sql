4c71883f04d1af7722451e6fd4ced1b42fa73137
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 16:10:52 +0100

    Beautified code for no_epp_per_modern_state_year SQL scripts

--- budé/sql_queries/no_epp_per_modern_state_year/no_epp_to_budé_from_modern_state_per_year.sql
+++ budé/sql_queries/no_epp_per_modern_state_year/no_epp_to_budé_from_modern_state_per_year.sql
@@ -1,1 +1,11 @@
-SELECT DISTINCT locations.locations_modern_state AS 'Modern State', COUNT(*) AS 'Number of letters sent from this modern state to Budé this year', send_date_year1 FROM letters, locations WHERE locations.locations_id = letters.source_loc_id AND letters_id NOT LIKE '%ck2' AND recipient_id = 'budé_guillaume_viaf_105878228' GROUP BY locations_modern_state, send_date_year1 ORDER BY send_date_year1 ASC
+SELECT DISTINCT locations.locations_modern_state AS 'Modern State',
+                COUNT(*) AS 'Number of letters sent from this modern state to Budé this year',
+                send_date_year1
+FROM letters,
+     locations
+WHERE locations.locations_id = letters.source_loc_id
+  AND letters_id NOT LIKE '%ck2'
+  AND recipient_id = 'budé_guillaume_viaf_105878228'
+GROUP BY locations_modern_state,
+         send_date_year1
+ORDER BY send_date_year1 ASC
