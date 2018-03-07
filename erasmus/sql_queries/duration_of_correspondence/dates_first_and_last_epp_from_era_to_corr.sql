f1298a22955e1f6c7821b9fb612ebf52f9a5c343
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 12:51:35 +0100

    prefixed duration_of_correspondence queries with db name

--- erasmus/sql_queries/duration_of_correspondence/dates_first_and_last_epp_from_era_to_corr.sql
+++ erasmus/sql_queries/duration_of_correspondence/dates_first_and_last_epp_from_era_to_corr.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT recipient_id, MIN(send_date_computable1) AS 'First Letter From Erasmus', MAX(send_date_computable1) AS 'Last Letter From Erasmus' FROM letters WHERE sender_id = 'erasmus_desiderius_viaf_95982394' GROUP BY recipient_id
+SELECT DISTINCT recipient_id, MIN(send_date_computable1) AS 'First Letter From Erasmus', MAX(send_date_computable1) AS 'Last Letter From Erasmus' FROM era_cdb_v3.letters WHERE sender_id = 'erasmus_desiderius_viaf_95982394' GROUP BY recipient_id
