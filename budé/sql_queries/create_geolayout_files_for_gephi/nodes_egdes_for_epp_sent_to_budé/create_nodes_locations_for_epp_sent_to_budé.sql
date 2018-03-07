eb6b38a784f0fe31efa5f53d7ecde61958c91aba
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-07 15:58:37 +0100

    Beautified code for create_geolayout_files_for_gephi SQL scripts

--- budé/sql_queries/create_geolayout_files_for_gephi/nodes_egdes_for_epp_sent_to_budé/create_nodes_locations_for_epp_sent_to_budé.sql
+++ budé/sql_queries/create_geolayout_files_for_gephi/nodes_egdes_for_epp_sent_to_budé/create_nodes_locations_for_epp_sent_to_budé.sql
@@ -1,1 +1,22 @@
-SELECT locations_id AS 'Id', geonames_id, locations_name_modern, locations_name_in_edition, locations_modern_state, locations_modern_province, locations_lat, locations_lng, locations_ll_combined, locations_further_annotation FROM locations WHERE locations_id IN (SELECT DISTINCT source_loc_id FROM letters WHERE recipient_id = 'budé_guillaume_viaf_105878228' AND source_loc_id NOT LIKE 'unknown%') OR locations_id IN (SELECT DISTINCT target_loc_id FROM letters WHERE recipient_id = 'budé_guillaume_viaf_105878228' AND target_loc_id NOT LIKE 'unknown%') GROUP BY locations_id
+SELECT locations_id AS 'Id',
+       geonames_id,
+       locations_name_modern,
+       locations_name_in_edition,
+       locations_modern_state,
+       locations_modern_province,
+       locations_lat,
+       locations_lng,
+       locations_ll_combined,
+       locations_further_annotation
+FROM locations
+WHERE locations_id IN
+    (SELECT DISTINCT source_loc_id
+     FROM letters
+     WHERE recipient_id = 'budé_guillaume_viaf_105878228'
+       AND source_loc_id NOT LIKE 'unknown%')
+  OR locations_id IN
+    (SELECT DISTINCT target_loc_id
+     FROM letters
+     WHERE recipient_id = 'budé_guillaume_viaf_105878228'
+       AND target_loc_id NOT LIKE 'unknown%')
+GROUP BY locations_id
