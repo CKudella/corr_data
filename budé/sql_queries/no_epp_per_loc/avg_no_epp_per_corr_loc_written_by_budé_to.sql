7a7babd20aa98a148ed074db55ca941fb8c98e1a
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-08 16:31:45 +0100

    query syntax fix

--- budé/sql_queries/no_epp_per_loc/avg_no_epp_per_corr_loc_written_by_budé_to.sql
+++ budé/sql_queries/no_epp_per_loc/avg_no_epp_per_corr_loc_written_by_budé_to.sql
@@ -1,8 +1,8 @@
-SELECT y.locations_name_modern AS 'Location Name',
-       x.locations_lat AS 'Latitude',
-       x.locations_lng AS 'Longitude',
-       x.count AS 'Number of correspondents Budé writes to',
-       y.count AS 'Number of letters written by Budé',
+SELECT Y.locations_name_modern AS 'Location Name',
+       X.locations_lat AS 'Latitude',
+       X.locations_lng AS 'Longitude',
+       X.count AS 'Number of correspondents Budé writes to',
+       Y.count AS 'Number of letters written by Budé',
        Y.COUNT/X.COUNT AS 'Average number of letters'
 FROM
   (SELECT locations_name_modern,
@@ -10,7 +10,7 @@ FROM
           locations_lng,
           COUNT(DISTINCT recipient_id) AS COUNT
    FROM letters
-   JOIN budé_cdb_v1.locations ON locations.locations_id = letters.target_loc_id
+   JOIN locations ON locations.locations_id = letters.target_loc_id
    WHERE sender_id = 'budé_guillaume_viaf_105878228'
    GROUP BY target_loc_id
    ORDER BY COUNT DESC) AS X
