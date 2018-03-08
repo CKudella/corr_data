05cd07fb47e217e5f37dfec93c462cd7098658c4
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-08 21:15:50 +0100

    Added new SQL query for duration_of_correspondence

---
+++ budé/sql_queries/duration_of_correspondence/duration_of_correspodence_all_corr.sql
@@ -0,0 +1,22 @@
+SELECT C.correspondents_id,
+       C.name_in_edition,
+       LEAST(IFNULL(X.FLFE, Y.FLTE), IFNULL(Y.FLTE, X.FLFE)) AS 'Beginning of correspondence with Budé',
+       GREATEST(IFNULL(X.LLFE, Y.LLTE), IFNULL(Y.LLTE, X.LLFE)) AS 'End of the correspondence with Budé'
+FROM
+  (SELECT DISTINCT CA.correspondents_id,
+                   CA.name_in_edition
+   FROM correspondents AS CA) AS C
+LEFT OUTER JOIN
+  (SELECT DISTINCT XA.recipient_id,
+                   MIN(XA.send_date_computable1) AS FLFE,
+                   MAX(XA.send_date_computable1) AS LLFE
+   FROM budé_cdb_v1.letters AS XA
+   WHERE XA.sender_id = 'budé_guillaume_viaf_105878228'
+   GROUP BY XA.recipient_id) AS X ON C.correspondents_id = X.recipient_id
+LEFT OUTER JOIN
+  (SELECT DISTINCT YA.sender_id,
+                   MIN(YA.send_date_computable1) AS FLTE,
+                   MAX(YA.send_date_computable1) AS LLTE
+   FROM budé_cdb_v1.letters AS YA
+   WHERE YA.recipient_id = 'budé_guillaume_viaf_105878228'
+   GROUP BY YA.sender_id) AS Y ON C.correspondents_id = Y.sender_id
---
+++ pirckheimer/sql_queries/duration_of_correspondence/duration_of_correspodence_all_corr.sql
@@ -0,0 +1,22 @@
+SELECT C.correspondents_id,
+       C.name_in_edition,
+       LEAST(IFNULL(X.FLFE, Y.FLTE), IFNULL(Y.FLTE, X.FLFE)) AS 'Beginning of correspondence with Pirckheimer',
+       GREATEST(IFNULL(X.LLFE, Y.LLTE), IFNULL(Y.LLTE, X.LLFE)) AS 'End of the correspondence with Pirckheimer'
+FROM
+  (SELECT DISTINCT CA.correspondents_id,
+                   CA.name_in_edition
+   FROM correspondents AS CA) AS C
+LEFT OUTER JOIN
+  (SELECT DISTINCT XA.recipient_id,
+                   MIN(XA.send_date_computable1) AS FLFE,
+                   MAX(XA.send_date_computable1) AS LLFE
+   FROM wpirck_cdb_v1.letters AS XA
+   WHERE XA.sender_id = 'pirckheimer_willibald_viaf_27173507'
+   GROUP BY XA.recipient_id) AS X ON C.correspondents_id = X.recipient_id
+LEFT OUTER JOIN
+  (SELECT DISTINCT YA.sender_id,
+                   MIN(YA.send_date_computable1) AS FLTE,
+                   MAX(YA.send_date_computable1) AS LLTE
+   FROM wpirck_cdb_v1.letters AS YA
+   WHERE YA.recipient_id = 'pirckheimer_willibald_viaf_27173507'
+   GROUP BY YA.sender_id) AS Y ON C.correspondents_id = Y.sender_id
