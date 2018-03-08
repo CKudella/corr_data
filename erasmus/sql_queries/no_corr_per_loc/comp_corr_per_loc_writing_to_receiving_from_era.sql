930fd686a325992220cd78a29794f8e7815cfbe5
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-08 14:37:23 +0100

    fixed comp_corr_per_loc_writing_to_receiving_from_era.sql

--- erasmus/sql_queries/no_corr_per_loc/comp_corr_per_loc_writing_to_receiving_from_era.sql
+++ erasmus/sql_queries/no_corr_per_loc/comp_corr_per_loc_writing_to_receiving_from_era.sql
@@ -1,28 +1,31 @@
 SELECT Z.locations_id,
-       X.LocationName,
-       X.Latitude,
-       X.Longitude,
+       Z.LocationName AS 'Location Name Modern',
+       Z.Latitude,
+       Z.Longitude,
        X.NoCorrToEra,
        Y.NoCorrFromEra
 FROM
-  (SELECT ZA.locations_id
-   FROM locations as ZA) AS Z
-INNER JOIN
+  (SELECT ZA.locations_id,
+          ZA.locations_name_modern LocationName,
+          ZA.locations_lat AS Latitude,
+          ZA.locations_lng AS Longitude
+   FROM locations AS ZA
+   WHERE ZA.locations_id IN
+       (SELECT DISTINCT source_loc_id
+        FROM letters)
+     OR ZA.locations_id IN
+       (SELECT DISTINCT target_loc_id
+        FROM letters)) AS Z
+LEFT OUTER JOIN
   (SELECT XB.locations_id,
-          XB.locations_name_modern AS 'LocationName',
-          XB.locations_lat AS Latitude,
-          XB.locations_lng AS Longitude,
           COUNT(DISTINCT XA.sender_id) AS NoCorrToEra
    FROM letters AS XA
-   JOIN locations AS XB  ON XB.locations_id = XA.source_loc_id
+   JOIN locations AS XB ON XB.locations_id = XA.source_loc_id
    WHERE XA.sender_id != 'erasmus_desiderius_viaf_95982394'
    GROUP BY XA.source_loc_id
    ORDER BY COUNT(DISTINCT sender_id) DESC) AS X ON X.locations_id = Z.locations_id
-INNER JOIN
+LEFT OUTER JOIN
   (SELECT YB.locations_id,
-          YB.locations_name_modern AS 'LocationName',
-          YB.locations_lat AS Latitude,
-          YB.locations_lng AS Longitude,
           COUNT(DISTINCT YA.recipient_id) AS NoCorrFromEra
    FROM letters AS YA
    JOIN locations AS YB ON YB.locations_id = YA.target_loc_id
