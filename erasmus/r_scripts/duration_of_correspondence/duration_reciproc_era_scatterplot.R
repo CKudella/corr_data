f4b66bcfb3ad0db4f0ff90a7f25bb45ca95a59b2
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-10 15:52:58 +0100

    Added some r_scripts; removed faulty queries from duration_of_correspondence and pertaining query_results

---
+++ erasmus/r_scripts/duration_of_correspondence/duration_reciproc_era_scatterplot.R
@@ -0,0 +1,14 @@
+library(readr)
+library(timelineS)
+# set working directory
+getwd()
+setwd("../query_results/")
+
+# read data and define data type for date columns
+data<-read.csv("duration_of_correspondence/dates_begin_end_reciproc_correspondence.csv", fileEncoding="UTF-8", colClasses=c("Start.of.correspondence"="Date","End.of.correspondence"="Date"))
+
+# calculate duration of correspondence
+data$duration <- durCalc(data, start="Start.of.correspondence", end="End.of.correspondence", timeunit="years")
+
+#durPlot Plot Duration of Correspondence
+durPlot(df=data, start="Start.of.correspondence", end="End.of.correspondence", plot_type="scatter", timeunit = "years")
--- erasmus/sql_queries/duration_of_correspondence/dates_first_and_last_epp_from_era_to_corr.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT recipient_id,
-                MIN(send_date_computable1) AS 'First letter from Erasmus',
-                MAX(send_date_computable1) AS 'Last letter from Erasmus'
-FROM era_cdb_v3.letters
-WHERE sender_id = 'erasmus_desiderius_viaf_95982394'
-GROUP BY recipient_id
