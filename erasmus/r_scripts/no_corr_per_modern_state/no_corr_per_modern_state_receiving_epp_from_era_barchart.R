d3b6852d696f8264f7687e7b4806b9fa290679f3
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-11 07:47:37 +0100

    Reworked R Scripts

--- erasmus/r_scripts/no_corr_per_modern_state/no_corr_per_modern_state_receiving_epp_from_era_barchart.R
+++ erasmus/r_scripts/no_corr_per_modern_state/no_corr_per_modern_state_receiving_epp_from_era_barchart.R
@@ -1,14 +1,16 @@
-library(ggplot2)
+require(readr)
+require(ggplot2)
 library(readr)
-# Daten aus CSV laden und als Dataframe 'daten' zur Verfügung stellen
-daten<-read.csv("data/comp_correspondents_per_modern_state.csv")
+library(ggplot2)
 
-# Dataframe nach Spalte 'Number.of.Correspondents.who.received.letters.BY.Erasmus'sortieren
-daten[order(daten$Number.of.Correspondents.who.received.letters.BY.Erasmus)]
+# read data
+data<-read.csv("no_corr_per_modern_state/no_corr_per_modern_state_receiving_epp_from_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
-
-# Barchart aus dem Datenframe erstellen und x-Achse nach den Werten aus y.Achse sortieren, Barlabels hinzufügen, Achsenlabels umbenenenen
-plot <- ggplot(data=daten, aes(x= reorder(Modern.State, -Number.of.Correspondents.who.received.letters.BY.Erasmus),y=Number.of.Correspondents.who.received.letters.BY.Erasmus)) + geom_bar(stat = "identity") + geom_text(aes(label=Number.of.Correspondents.who.received.letters.BY.Erasmus), vjust=-0.5, color='black') + labs(x="Modern State",y="Number of correspondents receiving letters from Erasmus") 
-
-# X-Achsen Labels rotieren
-plot + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.35))
+# create linechart
+plot <- ggplot(data, aes(x= reorder(Modern.State, -Number.of.correspondents.who.received.letters.from.Erasmus),y=Number.of.correspondents.who.received.letters.from.Erasmus)) + 
+  geom_bar(stat = "identity") +
+  geom_text(aes(label=Number.of.correspondents.who.received.letters.from.Erasmus), vjust=-0.5, color='black') +
+  labs(x="Modern State",y="Number of correspondents") +
+  theme_bw() + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.35))
+plot
--- erasmus/r_scripts/no_corr_per_modern_state/no_corr_per_modern_state_writing_epp_to_era_barchart.R
+++ erasmus/r_scripts/no_corr_per_modern_state/no_corr_per_modern_state_writing_epp_to_era_barchart.R
@@ -1,14 +1,16 @@
-library(ggplot2)
+require(readr)
+require(ggplot2)
 library(readr)
-# Daten aus CSV laden und als Dataframe 'daten' zur Verfügung stellen
-daten<-read.csv("data/comp_no_corr_per_modern_state.csv")
+library(ggplot2)
 
-# Dataframe nach Spalte 'Number.of.Correspondents.who.wrote.letters.TO.Erasmus'sortieren
-daten[order(daten$Number.of.Correspondents.who.wrote.letters.TO.Erasmus)]
-daten
+# read data
+data<-read.csv("no_corr_per_modern_state/no_corr_per_modern_state_writing_epp_to_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
-# Barchart aus dem Datenframe erstellen und x-Achse nach den Werten aus y.Achse sortieren, Barlabels hinzufügen, Achsenlabels umbenenenen
-plot <- ggplot(data=daten, aes(x= reorder(Modern.State, -Number.of.Correspondents.who.wrote.letters.TO.Erasmus),y=Number.of.Correspondents.who.wrote.letters.TO.Erasmus)) + geom_bar(stat = "identity") + geom_text(aes(label=Number.of.Correspondents.who.wrote.letters.TO.Erasmus), vjust=-0.5, color='black') + labs(x="Modern State",y="Number of correspondents who wrote letters to Erasmus") 
-
-# X-Achsen Labels rotieren
-plot + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.35))
+# create linechart
+plot <- ggplot(data, aes(x= reorder(Modern.State, -Number.of.correspondents.who.wrote.letters.to.Erasmus),y=Number.of.correspondents.who.wrote.letters.to.Erasmus)) + 
+  geom_bar(stat = "identity") +
+  geom_text(aes(label=Number.of.correspondents.who.wrote.letters.to.Erasmus), vjust=-0.5, color='black') +
+  labs(x="Modern State",y="Number of correspondents") +
+  theme_bw() + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.35))
+plot
