abf7ab4eb8f0ba6b51db958f2c3263601e775b07
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-11 15:23:32 +0100

    Added and reworked R scripts

--- erasmus/r_scripts/no_epp_per_year/no_epp_per_year_with_non_inferred_send_date_barchart.R
+++ erasmus/r_scripts/no_epp_per_year/no_epp_per_year_with_non_inferred_send_date_barchart.R
@@ -6,12 +6,12 @@ library(reshape2)
 library(ggplot2)
 
 # read data
-data<-read.csv("no_epp_per_year/no_epp_per_year_send_date_year1.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+data<-read.csv("no_epp_per_year/no_epp_per_year_with_non_inferred_send_date.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
 # create barchart
-plot <- ggplot(data=data, aes(x= send_date_year1,y=Total.number.of.letters.sent.this.year)) + 
+plot <- ggplot(data=data, aes(x=Year,y=Number.of.letters.with.non.inferred.send.date.sent.this.year)) + 
   geom_bar(stat = "identity") + 
-  geom_text(aes(label=Total.number.of.letters.sent.this.year), vjust=-0.5, color='black', size = 2.5) +
+  geom_text(aes(label=Number.of.letters.with.non.inferred.send.date.sent.this.year), vjust=-0.5, color='black', size = 2.5) +
   labs(x="Year",y="Number of letters") +
   scale_x_continuous(breaks = c(1484:1536)) +
   scale_y_continuous(breaks = seq(0,240,10)) +
