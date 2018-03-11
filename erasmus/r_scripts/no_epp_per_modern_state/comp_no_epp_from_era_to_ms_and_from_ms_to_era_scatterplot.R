c1c5bcb3aacae62c9c5b51b8fad679a059a11767
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-11 11:53:51 +0100

    Added new R script

---
+++ erasmus/r_scripts/no_epp_per_modern_state/comp_no_epp_from_era_to_ms_and_from_ms_to_era_scatterplot.R
@@ -0,0 +1,17 @@
+require(readr)
+require(reshape2)
+require(ggplot2)
+library(readr)
+library(reshape2)
+library(ggplot2)
+
+# # read data
+data<-read.csv("no_epp_per_modern_state/comp_no_epp_from_era_to_ms_and_from_ms_to_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+
+# create scatterplot
+plot <- ggplot(data=data, aes(x=Number.of.letters.sent.from.this.modern.state.to.Erasmus, y=Number.of.letters.Erasmus.sent.to.this.modern.state, label=ModernState)) + 
+  geom_point(stat = "identity") +
+  geom_smooth(method = lm) +
+labs(x="Number.of.letters.sent.from.this.modern.state.to.Erasmus",y="Number.of.letters.Erasmus.sent.to.this.modern.state") + 
+  theme_bw()
+plot
