7743a065806b6ae91448d116cdc47cc1d24cc6ed
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-11 12:58:26 +0100

    Reworked R script

--- erasmus/r_scripts/no_epp_per_modern_state_year/comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.R
+++ erasmus/r_scripts/no_epp_per_modern_state_year/comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.R
@@ -2,14 +2,21 @@ library(reshape2)
 library(ggplot2)
 library(readr)
 
-# Facet Grid visualization of the number of letters Erasmus sent to the modern states compared to the number of letters he received from there der Briefe von Erasmus an die modern states pro Jahr
+# read data
+data<-read.csv("no_epp_per_modern_state_year/comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
-# Create Dataframe
-daten<-read.csv("data/no_epp_from_to_era_per_modern_state_per_year.csv")
+# apply melt for wide to long
+data_long <- melt(data, id.vars= c("ModernState","Year"))
 
-# Melt from Wide zu Long
-daten2 <- melt(daten, id.vars= c("ModernState","send_date_year1"))
 
 # Plot Facet Grid
-plot <- ggplot(data=daten2, aes(x=send_date_year1,y=value, colour=variable)) + geom_line(stat = "identity") + labs(x="Year",y="Number of letters") + scale_x_continuous(breaks = c(1484:1536)) + facet_grid(ModernState ~ ., space = "free" )
-plot + theme_minimal() + theme(axis.text.x = element_text(angle = 90, vjust = 0.35)) + theme(strip.text.y = element_text(angle = 0, hjust = 1)) + theme(legend.position="bottom") + scale_color_grey(labels = c("Number of letters sent from Erasmus to the modern state", "Number of letters sent from mondern state to Erasmus"))
+plot <- ggplot(data=data_long, aes(x=Year,y=value, colour=variable)) + 
+  geom_line(stat = "identity") + 
+  labs(x="Year",y="Number of letters") + 
+  scale_x_continuous(breaks = c(1484:1536)) + 
+  facet_grid(ModernState ~ ., space = "free" ) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.35)) +
+  theme(strip.text.y = element_text(angle = 0, hjust = 1)) +
+  theme(legend.position="bottom")
+plot
