e36088ef18d6bd4ee153a01c953163f440cfb9a1
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-11 14:31:48 +0100

    Reworked R scripts + Rename

---
+++ erasmus/r_scripts/no_epp_per_modern_state_year/no_epp_to_era_from_modern_state_per_year_facet_grid.R
@@ -0,0 +1,21 @@
+require(readr)
+require(reshape2)
+require(ggplot2)
+library(readr)
+library(reshape2)
+library(ggplot2)
+
+# read data
+data<-read.csv("no_epp_per_modern_state_year/no_epp_to_era_from_modern_state_per_year.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+
+# plot facet grid
+plot <- ggplot(data=data, aes(x=send_date_year1,y=Number.of.letters.sent.from.this.modern.state.to.Erasmus.this.year)) + 
+  geom_bar(stat = "identity") + 
+  labs(x="Year",y="Number of letters") + 
+  scale_x_continuous(breaks = c(1484:1536)) +
+  facet_grid(Modern.State ~ ., space = "free" )+
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.35)) +
+  theme(strip.text.y = element_text(angle = 0, hjust = 1)) + 
+  theme(legend.position="bottom")
+plot
