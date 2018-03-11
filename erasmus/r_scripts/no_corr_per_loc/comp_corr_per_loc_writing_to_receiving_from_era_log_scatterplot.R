7cde79631b9dfebf8c2179ee2b38a3e0be3d1b22
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-11 10:25:04 +0100

    Added R script

---
+++ erasmus/r_scripts/no_corr_per_loc/comp_corr_per_loc_writing_to_receiving_from_era_log_scatterplot.R
@@ -0,0 +1,17 @@
+require(readr)
+require(ggplot2)
+library(readr)
+library(ggplot2)
+
+# read data
+data<-read.csv("no_corr_per_loc/comp_corr_per_loc_writing_to_receiving_from_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+
+# create scatterplot
+plot <- ggplot(data=data, aes(x=NoCorrToEra, y=NoCorrFromEra, label=Location.Name.Modern)) + 
+  geom_point(stat = "identity") +
+  geom_smooth(method = lm) +
+  scale_x_continuous(trans='log2') +
+  scale_y_continuous(trans='log2')
+labs(x="NoCorrToEra",y="NoCorrFromEra") + 
+  theme_bw()
+plot
