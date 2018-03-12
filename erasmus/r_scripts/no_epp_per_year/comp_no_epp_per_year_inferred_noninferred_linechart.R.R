807c45c50425e5fbd64ce83b48f0b728e6f89ad0
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-12 14:48:20 +0100

    Reworked and renamed R script

---
+++ erasmus/r_scripts/no_epp_per_year/comp_no_epp_per_year_inferred_noninferred_linechart.R.R
@@ -0,0 +1,29 @@
+require(readr)
+require(reshape2)
+require(ggplot2)
+library(readr)
+library(reshape2)
+library(ggplot2)
+
+# set working directory
+getwd()
+setwd("../query_results/")
+
+# read data
+data<-read.csv("no_epp_per_year/comp_no_epp_per_year_inferred_noninferred.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+
+
+# apply melt for wide to long
+data_long <- melt(data, id.vars= c("Year"))
+
+# create linechart
+plot <- ggplot(data=data_long, aes(x= Year, y=value, colour=variable)) +
+  geom_line(stat = "identity", size=0.9) +
+  geom_point(shape=1) + labs(x="Year",y="Number of letters") +
+  scale_x_continuous(breaks = c(1484:1536)) +
+  scale_y_continuous(breaks = seq(0,160,10)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.35)) +
+  theme(legend.position="bottom") +
+  scale_color_grey(labels = c("Number of letters with inferred send data", "Number of letters with non-inferred send data"))
+plot
