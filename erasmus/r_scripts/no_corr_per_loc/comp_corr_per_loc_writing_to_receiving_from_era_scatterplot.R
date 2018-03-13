490b83062c50857782b3a11a31edba95eb6a8064
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-13 14:20:21 +0100

    Reworked R script; Generated plots

---
+++ erasmus/r_scripts/no_corr_per_loc/comp_corr_per_loc_writing_to_receiving_from_era_scatterplot.R
@@ -0,0 +1,29 @@
+require(readr)
+require(ggplot2)
+library(readr)
+library(ggplot2)
+
+# set working directory
+getwd()
+setwd("../query_results/")
+
+# read data
+data<-read.csv("no_corr_per_loc/comp_corr_per_loc_writing_to_receiving_from_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+
+# create scatterplot
+plot <- ggplot(data=data, aes(x=NoCorrToEra, y=NoCorrFromEra, label=Location.Name.Modern)) + 
+  geom_point(stat = "identity") +
+  geom_smooth(method = lm) +
+  labs(x="Number of correspondents writing letters to Erasmus",y="Number of Correspondents receiving letters from Erasmus") + 
+  theme_bw()
+plot
+
+# change working directory
+getwd()
+setwd("../r_plots/")
+
+# save plot in multiple formats
+ggsave("comp_corr_per_loc_writing_to_receiving_from_era_scatterplot.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("comp_corr_per_loc_writing_to_receiving_from_era_scatterplot.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("comp_corr_per_loc_writing_to_receiving_from_era_scatterplot.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("comp_corr_per_loc_writing_to_receiving_from_era_scatterplot.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
