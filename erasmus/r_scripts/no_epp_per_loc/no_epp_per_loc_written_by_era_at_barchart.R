e262d47e369e8a2289c0a6fe2314f4b67509fd18
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-14 14:08:50 +0100

    Added two R script and pertaining plots; Removed one R script and plots

---
+++ erasmus/r_scripts/no_epp_per_loc/no_epp_per_loc_written_by_era_at_barchart.R
@@ -0,0 +1,30 @@
+require(readr)
+require(ggplot2)
+library(readr)
+library(ggplot2)
+
+# set working directory
+getwd()
+setwd("../query_results/")
+
+# read data
+data<-read.csv("no_epp_per_loc/no_epp_per_loc_written_by_era_at.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+
+# create scatterplot
+plot <- ggplot(data=data, aes(x= reorder(Location.Name, -Number.of.letters.sent.from.this.location.from.Erasmus), y=Number.of.letters.sent.from.this.location.from.Erasmus, label=Location.Name)) + 
+  geom_bar(stat = "identity") + 
+  labs(x="Locations",y="Number of letters sent from this location by Erasmus") +
+  geom_text(aes(label=Number.of.letters.sent.from.this.location.from.Erasmus), vjust=-0.5, color='black') +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.35))
+plot
+
+# change working directory
+getwd()
+setwd("../r_plots/")
+
+# save plot in multiple formats
+ggsave("no_epp_per_loc_written_by_era_at_barchart.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_loc_written_by_era_at_barchart.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_loc_written_by_era_at_barchart.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_loc_written_by_era_at_barchart.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
