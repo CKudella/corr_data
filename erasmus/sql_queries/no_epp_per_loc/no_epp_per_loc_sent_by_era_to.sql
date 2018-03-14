128e30e48c9732e3bc88e9d0af282d297e2868c0
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-14 21:12:14 +0100

    Added new SQL queries

---
+++ erasmus/sql_queries/no_epp_per_loc/no_epp_per_loc_sent_by_era_to.sql
@@ -0,0 +1,9 @@
+SELECT locations.locations_name_modern AS 'Location Name',
+       COUNT(letters.target_loc_id) AS 'Number of letters sent to this location from Erasmus'
+FROM letters
+JOIN locations ON locations.locations_id = letters.target_loc_id
+WHERE letters_id NOT LIKE '%ck2'
+  AND sender_id = 'erasmus_desiderius_viaf_95982394'
+  AND target_loc_id NOT LIKE 'unknown%'
+GROUP BY target_loc_id
+ORDER BY COUNT(letters.target_loc_id) DESC
---
+++ erasmus/sql_queries/no_epp_per_loc/no_epp_per_loc_sent_to_era.sql
@@ -0,0 +1,9 @@
+SELECT locations.locations_name_modern AS 'Location Name',
+       COUNT(letters.source_loc_id) AS 'Number of letters sent from this location to Erasmus'
+FROM letters
+JOIN locations ON locations.locations_id = letters.source_loc_id
+WHERE letters_id NOT LIKE '%ck2'
+  AND recipient_id = 'erasmus_desiderius_viaf_95982394'
+  AND source_loc_id NOT LIKE 'unknown%'
+GROUP BY source_loc_id
+ORDER BY COUNT(letters.source_loc_id) DESC
