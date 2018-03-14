c2715401832f8028f196ed8a0c31a70c1fc78aa2
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-14 16:53:41 +0100

    Added new SQL queries

---
+++ erasmus/sql_queries/no_epp_per_loc/avg_no_epp_per_loc_year_written_to_era.sql
@@ -0,0 +1,25 @@
+SELECT Y.locations_name_modern AS 'Location Name',
+       X.locations_lat AS 'Latitude',
+       X.locations_lng AS 'Longitude',
+       X.count AS 'Number of years letters are written from this location to Erasmus',
+       Y.count AS 'Number of letters written from this location to Erasmus',
+       Y.COUNT/X.COUNT AS 'Average Number of Letters written from this location to Erasmus per year'
+FROM
+  (SELECT locations_name_modern,
+          locations_lat,
+          locations_lng,
+          COUNT(DISTINCT send_date_year1) AS COUNT
+   FROM letters
+   JOIN era_cdb_v3.locations ON locations.locations_id = letters.source_loc_id
+   WHERE recipient_id = 'erasmus_desiderius_viaf_95982394'
+   GROUP BY source_loc_id
+   ORDER BY COUNT DESC) AS X
+INNER JOIN
+  (SELECT locations.locations_name_modern,
+          COUNT(letters.target_loc_id) AS COUNT
+   FROM letters
+   JOIN locations ON locations.locations_id = letters.source_loc_id
+   WHERE letters_id NOT LIKE '%ck2'
+     AND recipient_id = 'erasmus_desiderius_viaf_95982394'
+   GROUP BY source_loc_id
+   ORDER BY COUNT DESC) AS Y ON X.locations_name_modern = Y.locations_name_modern
