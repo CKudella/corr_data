16f52cb51f1e49aad62632449e560f827ec05f3f
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-16 17:37:40 +0100

    Added new R script; Generated new plots

---
+++ erasmus/r_scripts/no_epp_per_loc/no_epp_per_loc_sent_to_era_outliers_barchart_facet_grid.R
@@ -0,0 +1,35 @@
+require(readr)
+require(reshape2)
+require(ggplot2)
+library(readr)
+library(reshape2)
+library(ggplot2)
+
+# set working directory
+getwd()
+setwd("../query_results/")
+
+# read data
+data<-read.csv("no_epp_per_loc/no_epp_per_loc_sent_to_era_outliers.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+
+# plot facet grid
+plot <- ggplot(data=data, aes(x=YEAR,y=COUNT)) + 
+  geom_bar(stat = "identity") + 
+  labs(x="Year",y="Number of letters sent to Erasmus from this location") + 
+  scale_x_continuous(breaks = c(1484:1536)) +
+  facet_grid(locations_name_modern ~ ., space = "free" )+
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.35)) +
+  theme(strip.text.y = element_text(angle = 0, hjust = 1)) + 
+  theme(legend.position="bottom")
+plot
+
+# change working directory
+getwd()
+setwd("../r_plots/")
+
+# save plot in multiple formats
+ggsave("no_epp_per_loc_sent_to_era_outliers_barchart_facet_grid.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_loc_sent_to_era_outliers_barchart_facet_grid.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_loc_sent_to_era_outliers_barchart_facet_grid.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_loc_sent_to_era_outliers_barchart_facet_grid.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
