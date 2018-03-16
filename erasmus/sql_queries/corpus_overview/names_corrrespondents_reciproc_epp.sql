9e0cb740cf178ebd549bbb1a681a3df21d10179d
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-16 17:57:43 +0100

    Added new SQL query

---
+++ erasmus/sql_queries/corpus_overview/names_corrrespondents_reciproc_epp.sql
@@ -0,0 +1,12 @@
+SELECT name_in_edition  AS 'Names of correspondents for whom the dataset contains a reciprocal correspondence with Erasmus'
+ FROM era_cdb_v3.correspondents
+ WHERE correspondents_id IN
+     (SELECT DISTINCT sender_id
+      FROM letters
+      WHERE recipient_id = 'erasmus_desiderius_viaf_95982394'
+        AND sender_id != 'unnamed_person_viaf_not_applicable')
+   AND correspondents_id IN
+     (SELECT DISTINCT recipient_id
+      FROM letters
+      WHERE sender_id = 'erasmus_desiderius_viaf_95982394'
+        AND recipient_id != 'unnamed_person_viaf_not_applicable')
