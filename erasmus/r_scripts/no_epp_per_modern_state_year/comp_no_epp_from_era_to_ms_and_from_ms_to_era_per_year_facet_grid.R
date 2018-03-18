8a3999336dac0ab8922dbaee685417edc0826212
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-18 21:37:20 +0100

    fixed some comments

--- erasmus/r_scripts/no_epp_per_modern_state_year/comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.R
+++ erasmus/r_scripts/no_epp_per_modern_state_year/comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.R
@@ -15,12 +15,11 @@ data<-read.csv("no_epp_per_modern_state_year/comp_no_epp_from_era_to_ms_and_from
 # apply melt for wide to long
 data_long <- melt(data, id.vars= c("ModernState","Year"))
 
-
-# Plot Facet Grid
-plot <- ggplot(data=data_long, aes(x=Year,y=value, colour=variable)) + 
-  geom_line(stat = "identity") + 
-  labs(x="Year",y="Number of letters") + 
-  scale_x_continuous(breaks = c(1484:1536)) + 
+# create barchart with facet grid
+plot <- ggplot(data=data_long, aes(x=Year,y=value, colour=variable)) +
+  geom_line(stat = "identity") +
+  labs(x="Year",y="Number of letters") +
+  scale_x_continuous(breaks = c(1484:1536)) +
   facet_grid(ModernState ~ ., space = "free" ) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.35)) +
@@ -37,4 +36,3 @@ ggsave("comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.pdf", 
 ggsave("comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
 ggsave("comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
 ggsave("comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-
