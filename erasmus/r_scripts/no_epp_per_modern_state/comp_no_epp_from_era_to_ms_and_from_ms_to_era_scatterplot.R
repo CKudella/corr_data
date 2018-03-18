94ef98a887cfdd9baaafe7d82fb48b78a05c8807
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-18 21:31:26 +0100

    Fixed axis labeling; Generated new plots

--- erasmus/r_scripts/no_epp_per_modern_state/comp_no_epp_from_era_to_ms_and_from_ms_to_era_scatterplot.R
+++ erasmus/r_scripts/no_epp_per_modern_state/comp_no_epp_from_era_to_ms_and_from_ms_to_era_scatterplot.R
@@ -13,10 +13,10 @@ setwd("../query_results/")
 data<-read.csv("no_epp_per_modern_state/comp_no_epp_from_era_to_ms_and_from_ms_to_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
 # create scatterplot
-plot <- ggplot(data=data, aes(x=Number.of.letters.sent.from.this.modern.state.to.Erasmus, y=Number.of.letters.Erasmus.sent.to.this.modern.state, label=ModernState)) + 
+plot <- ggplot(data=data, aes(x=Number.of.letters.sent.from.this.modern.state.to.Erasmus, y=Number.of.letters.Erasmus.sent.to.this.modern.state, label=ModernState)) +
   geom_point(stat = "identity") +
   geom_smooth(method = lm) +
-  labs(x="Number.of.letters.sent.from.this.modern.state.to.Erasmus",y="Number.of.letters.Erasmus.sent.to.this.modern.state") + 
+  labs(x="Number of letters sent from this modern state to Erasmus",y="Number of letters sent from Erasmus to this modern state") + 
   theme_bw()
 plot
 
