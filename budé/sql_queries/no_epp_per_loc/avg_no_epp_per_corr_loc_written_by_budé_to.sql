f961025bc6600a052169891feece3b8dedb3ecb2
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-18 17:55:04 +0100

    Altered queries to align with era query

--- budé/sql_queries/no_epp_per_loc/avg_no_epp_per_corr_loc_written_by_budé_to.sql
+++ budé/sql_queries/no_epp_per_loc/avg_no_epp_per_corr_loc_written_by_budé_to.sql
@@ -3,23 +3,26 @@ SELECT Y.locations_name_modern AS 'Location Name',
        X.locations_lng AS 'Longitude',
        X.count AS 'Number of correspondents Budé writes to',
        Y.count AS 'Number of letters written by Budé',
-       Y.COUNT/X.COUNT AS 'Average number of letters'
+       Y.COUNT/X.COUNT AS 'Average Number of letters per correspondent'
 FROM
   (SELECT locations_name_modern,
           locations_lat,
           locations_lng,
           COUNT(DISTINCT recipient_id) AS COUNT
-   FROM letters
-   JOIN locations ON locations.locations_id = letters.target_loc_id
-   WHERE sender_id = 'budé_guillaume_viaf_105878228'
+   FROM budé_cdb_v1.letters
+   JOIN budé_cdb_v1.locations ON locations.locations_id = letters.target_loc_id
+   WHERE letters_id NOT LIKE '%ck2'
+     AND sender_id = 'budé_guillaume_viaf_105878228'
+     AND target_loc_id NOT LIKE 'unknown%'
    GROUP BY target_loc_id
    ORDER BY COUNT DESC) AS X
 INNER JOIN
   (SELECT locations.locations_name_modern,
           COUNT(letters.target_loc_id) AS COUNT
-   FROM letters
-   JOIN locations ON locations.locations_id = letters.target_loc_id
+   FROM budé_cdb_v1.letters
+   JOIN budé_cdb_v1.locations ON locations.locations_id = letters.target_loc_id
    WHERE letters_id NOT LIKE '%ck2'
      AND sender_id = 'budé_guillaume_viaf_105878228'
+     AND target_loc_id NOT LIKE 'unknown%'
    GROUP BY target_loc_id
    ORDER BY COUNT DESC) AS Y ON X.locations_name_modern = Y.locations_name_modern
--- budé/sql_queries/no_epp_per_loc/avg_no_epp_per_corr_loc_written_to_budé.sql
+++ budé/sql_queries/no_epp_per_loc/avg_no_epp_per_corr_loc_written_to_budé.sql
@@ -3,23 +3,26 @@ SELECT Y.locations_name_modern AS 'Location Name',
        X.locations_lng AS 'Longitude',
        X.count AS 'Number of correspondents writing to Budé',
        Y.count AS 'Number of letters written to Budé',
-       Y.COUNT/X.COUNT AS 'Average number of letters'
+       Y.COUNT/X.COUNT AS 'Average Number of Letters'
 FROM
   (SELECT locations_name_modern,
           locations_lat,
           locations_lng,
           COUNT(DISTINCT sender_id) AS COUNT
-   FROM letters
-   JOIN locations ON locations.locations_id = letters.source_loc_id
-   WHERE sender_id != 'budé_guillaume_viaf_105878228'
+   FROM budé_cdb_v1.letters
+   JOIN budé_cdb_v1.locations ON locations.locations_id = letters.source_loc_id
+   WHERE letters_id NOT LIKE '%ck2'
+     AND recipient_id = 'budé_guillaume_viaf_105878228'
+     AND source_loc_id NOT LIKE 'unknown%'
    GROUP BY source_loc_id
    ORDER BY COUNT DESC) AS X
 INNER JOIN
   (SELECT locations.locations_name_modern,
-          COUNT(letters.source_loc_id) AS COUNT
-   FROM letters
-   JOIN locations ON locations.locations_id = letters.source_loc_id
+          COUNT(letters.target_loc_id) AS COUNT
+   FROM budé_cdb_v1.letters
+   JOIN budé_cdb_v1.locations ON locations.locations_id = letters.source_loc_id
    WHERE letters_id NOT LIKE '%ck2'
      AND recipient_id = 'budé_guillaume_viaf_105878228'
+     AND source_loc_id NOT LIKE 'unknown%'
    GROUP BY source_loc_id
    ORDER BY COUNT DESC) AS Y ON X.locations_name_modern = Y.locations_name_modern
