9140d7a7d3400d50a5557e6ff0fe84c2a138f2b2
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-18 20:45:35 +0100

    fixed some comments

--- erasmus/r_scripts/no_corr_per_loc/no_corr_per_loc_writing_to_era_pointplot.R
+++ erasmus/r_scripts/no_corr_per_loc/no_corr_per_loc_writing_to_era_pointplot.R
@@ -12,9 +12,9 @@ setwd("../query_results/")
 # read data
 data<-read.csv("no_corr_per_loc/no_corr_per_loc_writing_to_era_with_geocoordinates.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
-# create scatterplot
-plot <- ggplot(data=data, aes(x= reorder(Location.Name.Modern, -Number.of.correspondents.who.wrote.from.this.location.letters.to.Erasmus), y=Number.of.correspondents.who.wrote.from.this.location.letters.to.Erasmus, label=Location.Name.Modern)) + 
-  geom_point(stat = "identity") + 
+# create pointplot
+plot <- ggplot(data=data, aes(x= reorder(Location.Name.Modern, -Number.of.correspondents.who.wrote.from.this.location.letters.to.Erasmus), y=Number.of.correspondents.who.wrote.from.this.location.letters.to.Erasmus, label=Location.Name.Modern)) +
+  geom_point(stat = "identity") +
   labs(x="Locations",y="Number of correspondents writing letters to Erasmus") +
   theme_bw() +
   theme(axis.title.x=element_text(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
