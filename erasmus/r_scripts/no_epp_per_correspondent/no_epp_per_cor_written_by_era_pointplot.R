ed25a58c671c511c5ffe106ad24c6e640e5936f4
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-18 20:53:56 +0100

    fixed some comments; Generated new plots for label fix

--- erasmus/r_scripts/no_epp_per_correspondent/no_epp_per_cor_written_by_era_pointplot.R
+++ erasmus/r_scripts/no_epp_per_correspondent/no_epp_per_cor_written_by_era_pointplot.R
@@ -10,9 +10,9 @@ setwd("../query_results/")
 # read data
 data<-read.csv("no_epp_per_correspondent/no_epp_per_cor_written_by_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
-# create scatterplot
-plot <- ggplot(data=data, aes(x= reorder(recipient_id, -Number.of.letters.sent.from.Erasmus.to.this.correspondent), y=Number.of.letters.sent.from.Erasmus.to.this.correspondent, label=recipient_id)) + 
-  geom_point(stat = "identity") + 
+# create pointplot
+plot <- ggplot(data=data, aes(x= reorder(recipient_id, -Number.of.letters.sent.from.Erasmus.to.this.correspondent), y=Number.of.letters.sent.from.Erasmus.to.this.correspondent, label=recipient_id)) +
+  geom_point(stat = "identity") +
   labs(x="Correspondents",y="Number of letters received from from Erasmus") +
   theme_bw() +
   theme(axis.title.x=element_text(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
--- erasmus/r_scripts/no_epp_per_correspondent/no_epp_per_cor_written_to_era_pointplot.R
+++ erasmus/r_scripts/no_epp_per_correspondent/no_epp_per_cor_written_to_era_pointplot.R
@@ -10,10 +10,10 @@ setwd("../query_results/")
 # read data
 data<-read.csv("no_epp_per_correspondent/no_epp_per_cor_written_to_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
-# create scatterplot
-plot <- ggplot(data=data, aes(x= reorder(sender_id, -Number.of.letters.sent.to.Erasmus.from.this.correspondent), y=Number.of.letters.sent.to.Erasmus.from.this.correspondent, label=sender_id)) + 
-  geom_point(stat = "identity") + 
-  labs(x="Correspondents",y="Number of letters written Erasmus") +
+# create poinplot
+plot <- ggplot(data=data, aes(x= reorder(sender_id, -Number.of.letters.sent.to.Erasmus.from.this.correspondent), y=Number.of.letters.sent.to.Erasmus.from.this.correspondent, label=sender_id)) +
+  geom_point(stat = "identity") +
+  labs(x="Correspondents",y="Number of letters written to Erasmus") +
   theme_bw() +
   theme(axis.title.x=element_text(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
 plot
