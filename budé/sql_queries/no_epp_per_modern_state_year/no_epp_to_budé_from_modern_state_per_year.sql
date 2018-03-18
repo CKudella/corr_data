a491ff5301982d638b22e80bbe9b01a093a2527a
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-18 19:53:02 +0100

    prefixed tables

--- budé/sql_queries/no_epp_per_modern_state_year/no_epp_to_budé_from_modern_state_per_year.sql
+++ budé/sql_queries/no_epp_per_modern_state_year/no_epp_to_budé_from_modern_state_per_year.sql
@@ -1,8 +1,8 @@
 SELECT DISTINCT locations.locations_modern_state AS 'Modern State',
                 COUNT(*) AS 'Number of letters sent from this modern state to Budé this year',
                 send_date_year1
-FROM letters,
-     locations
+FROM budé_cdb_v1.letters,
+     budé_cdb_v1.locations
 WHERE locations.locations_id = letters.source_loc_id
   AND letters_id NOT LIKE '%ck2'
   AND recipient_id = 'budé_guillaume_viaf_105878228'
