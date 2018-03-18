2f361dbd25a5a77c1e62d5e95d3376ffa91514ea
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-18 19:56:12 +0100

    Added new SQL query

---
+++ budé/sql_queries/no_epp_per_modern_state_year/comp_no_epp_from_budé_to_ms_and_from_ms_to_budé_per_year.sql
@@ -0,0 +1,43 @@
+SELECT A.ModernState,
+       A.Year,
+       B.NoEppSentFromBudé,
+       C.NoEppSentToBudé
+FROM
+  (SELECT DISTINCT AA.locations_modern_state AS ModernState,
+                   AL.send_date_year1 AS YEAR
+   FROM budé_cdb_v1.locations AS AA,
+        budé_cdb_v1.letters AS AL
+   WHERE AA.locations_id IN
+       (SELECT DISTINCT AB.source_loc_id
+        FROM budé_cdb_v1.letters AS AB)
+     OR AA.locations_id IN
+       (SELECT DISTINCT AC.target_loc_id
+        FROM budé_cdb_v1.letters AS AC)
+   GROUP BY AL.send_date_year1,
+            AA.locations_modern_state) AS A
+LEFT OUTER JOIN
+  (SELECT DISTINCT locations.locations_modern_state AS ModernState,
+                   COUNT(*) AS NoEppSentFromBudé,
+                   send_date_year1
+   FROM budé_cdb_v1.letters,
+        budé_cdb_v1.locations
+   WHERE locations.locations_id = letters.target_loc_id
+     AND letters_id NOT LIKE '%ck2'
+     AND sender_id = 'budé_guillaume_viaf_105878228'
+   GROUP BY locations_modern_state,
+            send_date_year1
+   ORDER BY send_date_year1 ASC) AS B ON B.ModernState = A.ModernState
+AND B.send_date_year1 = A.Year
+LEFT OUTER JOIN
+  (SELECT DISTINCT locations.locations_modern_state AS ModernState,
+                   COUNT(*) AS NoEppSentToBudé,
+                   send_date_year1
+   FROM budé_cdb_v1.letters,
+        budé_cdb_v1.locations
+   WHERE locations.locations_id = letters.source_loc_id
+     AND letters_id NOT LIKE '%ck2'
+     AND recipient_id = 'budé_guillaume_viaf_105878228'
+   GROUP BY locations_modern_state,
+            send_date_year1
+   ORDER BY send_date_year1 ASC) AS C ON C.ModernState = A.ModernState
+AND C.send_date_year1 = A.Year
