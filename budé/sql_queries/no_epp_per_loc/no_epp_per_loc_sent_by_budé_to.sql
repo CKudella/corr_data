ac7a357343082dffd69f322f15f92449582e5cbf
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-18 19:47:31 +0100

    Added new SQL queries

---
+++ budé/sql_queries/no_epp_per_loc/no_epp_per_loc_sent_by_budé_to.sql
@@ -0,0 +1,9 @@
+SELECT locations.locations_name_modern AS 'Location Name',
+       COUNT(letters.target_loc_id) AS 'Number of letters sent to this location from Budé'
+FROM budé_cdb_v1.letters
+JOIN budé_cdb_v1.locations ON locations.locations_id = letters.target_loc_id
+WHERE letters_id NOT LIKE '%ck2'
+  AND sender_id = 'budé_guillaume_viaf_105878228'
+  AND target_loc_id NOT LIKE 'unknown%'
+GROUP BY target_loc_id
+ORDER BY COUNT(letters.target_loc_id) DESC
---
+++ budé/sql_queries/no_epp_per_loc/no_epp_per_loc_written_by_budé_at.sql
@@ -0,0 +1,9 @@
+SELECT locations.locations_name_modern AS 'Location Name',
+       COUNT(letters.source_loc_id) AS 'Number of letters sent from this location from Budé'
+FROM budé_cdb_v1.letters
+JOIN budé_cdb_v1.locations ON locations.locations_id = letters.source_loc_id
+WHERE letters_id NOT LIKE '%ck2'
+  AND sender_id = 'budé_guillaume_viaf_105878228'
+  AND source_loc_id NOT LIKE 'unknown%'
+GROUP BY source_loc_id
+ORDER BY COUNT(letters.source_loc_id) DESC
