8529d8f174de266e1c128458659019f30fd7ff73
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-18 20:10:29 +0100

    Various fixes and alignments to SQL queries

--- budé/sql_queries/no_epp_per_year/no_epp_per_year_send_date_year1.sql
+++ budé/sql_queries/no_epp_per_year/no_epp_per_year_send_date_year1.sql
@@ -1,5 +1,5 @@
-SELECT send_date_year1,
-       COUNT(*) AS 'Total number of letters'
-FROM letters
+SELECT DISTINCT send_date_year1,
+       COUNT(*) AS 'Total number of letters sent this year'
+FROM budé_cdb_v1.letters
 WHERE letters_id NOT LIKE '%ck2'
 GROUP BY send_date_year1
--- budé/sql_queries/no_epp_per_year/no_epp_per_year_sent_by_budé_send_date_year1.sql
+++ budé/sql_queries/no_epp_per_year/no_epp_per_year_sent_by_budé_send_date_year1.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT letters.send_date_year1,
-                COUNT(*) AS NoLetterSentByBudé
-FROM letters
+                COUNT(*) AS 'Total number of letters sent by Budé this year'
+FROM budé_cdb_v1.letters
 WHERE sender_id = 'budé_guillaume_viaf_105878228'
   AND letters_id NOT LIKE '%ck2'
 GROUP BY letters.send_date_year1
