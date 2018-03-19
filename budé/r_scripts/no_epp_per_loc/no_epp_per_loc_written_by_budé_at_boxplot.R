59bcd3099b79560ad9302b0232e99fee77c3f0b1
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-19 19:44:24 +0100

    minor adjustment to R script; Generated new plots

--- budé/r_scripts/no_epp_per_loc/no_epp_per_loc_written_by_budé_at_boxplot.R
+++ budé/r_scripts/no_epp_per_loc/no_epp_per_loc_written_by_budé_at_boxplot.R
@@ -15,7 +15,7 @@ data<-read.csv("no_epp_per_loc/no_epp_per_loc_written_by_budé_at.csv", fileEnco
 # create boxplot
 plot <- ggplot(data, aes(x= ' ', y = Number.of.letters.sent.from.this.location.from.Budé)) +
   geom_boxplot(outlier.size=2, notch = FALSE, show.legend = TRUE) +
-  geom_text_repel(label=ifelse(data$Number.of.letters.sent.from.this.location.from.Budé>9,as.character(data$Location.Name),'')) +
+  geom_text_repel(label=ifelse(data$Number.of.letters.sent.from.this.location.from.Budé>8.5,as.character(data$Location.Name),'')) +
   theme_bw() +
   theme(axis.title.x=element_blank()) +
   labs(y = "Number of letters sent from this location by Budé")
