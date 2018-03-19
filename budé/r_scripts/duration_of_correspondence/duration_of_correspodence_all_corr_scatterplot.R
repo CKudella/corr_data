c19e282d8b1ef92dbbb6080957dab2f7f5aa7ceb
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-19 15:01:01 +0100

    Adjusted R scripts to Budé; Generated new plots

--- budé/r_scripts/duration_of_correspondence/duration_of_correspodence_all_corr_scatterplot.R
+++ budé/r_scripts/duration_of_correspondence/duration_of_correspodence_all_corr_scatterplot.R
@@ -8,13 +8,13 @@ getwd()
 setwd("../query_results/")
 
 # read data and define data type for date columns
-data<-read.csv("duration_of_correspondence/duration_of_correspodence_all_corr.csv", fileEncoding="UTF-8", colClasses=c("Beginning.of.correspondence.with.Erasmus"="Date","End.of.the.correspondence.with.Erasmus"="Date"))
+data<-read.csv("duration_of_correspondence/duration_of_correspodence_all_corr.csv", fileEncoding="UTF-8", colClasses=c("Beginning.of.correspondence.with.Budé"="Date","End.of.the.correspondence.with.Budé"="Date"))
 
 # calculate duration of correspondence
-data$duration <- durCalc(data, start="Beginning.of.correspondence.with.Erasmus", end="End.of.the.correspondence.with.Erasmus", timeunit="years")
+data$duration <- durCalc(data, start="Beginning.of.correspondence.with.Budé", end="End.of.the.correspondence.with.Budé", timeunit="years")
 
 #durPlot (scatterplot) for duration of correspondence in correlation to the beginning of the correspondence
-durPlot(df=data, start="Beginning.of.correspondence.with.Erasmus", end="End.of.the.correspondence.with.Erasmus", plot_type="scatter", timeunit = "years", point_size = 1)
+durPlot(df=data, start="Beginning.of.correspondence.with.Budé", end="End.of.the.correspondence.with.Budé", plot_type="scatter", timeunit = "years", point_size = 1)
 
 # change working directory
 getwd()
