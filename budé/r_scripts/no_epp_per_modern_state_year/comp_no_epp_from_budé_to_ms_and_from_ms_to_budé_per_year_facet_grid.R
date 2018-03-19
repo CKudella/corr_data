c457a2e5f4cf482f5668fc8c5fdcc8a0caa991df
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-19 17:48:23 +0100

    Adjusted R scripts to Budé; Generated new plots

--- budé/r_scripts/no_epp_per_modern_state_year/comp_no_epp_from_budé_to_ms_and_from_ms_to_budé_per_year_facet_grid.R
+++ budé/r_scripts/no_epp_per_modern_state_year/comp_no_epp_from_budé_to_ms_and_from_ms_to_budé_per_year_facet_grid.R
@@ -10,7 +10,7 @@ getwd()
 setwd("../query_results/")
 
 # read data
-data<-read.csv("no_epp_per_modern_state_year/comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+data<-read.csv("no_epp_per_modern_state_year/comp_no_epp_from_budé_to_ms_and_from_ms_to_budé_per_year.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
 # apply melt for wide to long
 data_long <- melt(data, id.vars= c("ModernState","Year"))
@@ -19,7 +19,7 @@ data_long <- melt(data, id.vars= c("ModernState","Year"))
 plot <- ggplot(data=data_long, aes(x=Year,y=value, colour=variable)) +
   geom_line(stat = "identity") +
   labs(x="Year",y="Number of letters") +
-  scale_x_continuous(breaks = c(1484:1536)) +
+  scale_x_continuous(breaks = c(1484:1540)) +
   facet_grid(ModernState ~ ., space = "free" ) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.35)) +
@@ -32,7 +32,7 @@ getwd()
 setwd("../r_plots/")
 
 # save plot in multiple formats
-ggsave("comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("comp_no_epp_from_budé_to_ms_and_from_ms_to_budé_per_year_facet_grid.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("comp_no_epp_from_budé_to_ms_and_from_ms_to_budé_per_year_facet_grid.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("comp_no_epp_from_budé_to_ms_and_from_ms_to_budé_per_year_facet_grid.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("comp_no_epp_from_budé_to_ms_and_from_ms_to_budé_per_year_facet_grid.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
