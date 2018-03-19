b335c1018fa4315334f054c7e98b7a2221c56817
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-19 16:43:05 +0100

    Adjusted R scripts to Budé; Generated new plots

--- budé/r_scripts/no_epp_per_correspondent/no_epp_per_cor_written_by_budé_pointplot.R
+++ budé/r_scripts/no_epp_per_correspondent/no_epp_per_cor_written_by_budé_pointplot.R
@@ -8,12 +8,12 @@ getwd()
 setwd("../query_results/")
 
 # read data
-data<-read.csv("no_epp_per_correspondent/no_epp_per_cor_written_by_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+data<-read.csv("no_epp_per_correspondent/no_epp_per_cor_written_by_budé.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
 # create pointplot
-plot <- ggplot(data=data, aes(x= reorder(recipient_id, -Number.of.letters.sent.from.Erasmus.to.this.correspondent), y=Number.of.letters.sent.from.Erasmus.to.this.correspondent, label=recipient_id)) +
+plot <- ggplot(data=data, aes(x= reorder(recipient_id, -Number.of.letters.sent.from.Budé.to.this.correspondent), y=Number.of.letters.sent.from.Budé.to.this.correspondent, label=recipient_id)) +
   geom_point(stat = "identity") +
-  labs(x="Correspondents",y="Number of letters received from from Erasmus") +
+  labs(x="Correspondents",y="Number of letters received from from Budé") +
   theme_bw() +
   theme(axis.title.x=element_text(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
 plot
@@ -23,7 +23,7 @@ getwd()
 setwd("../r_plots/")
 
 # save plot in multiple formats
-ggsave("no_epp_per_cor_written_by_era_pointplot.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_epp_per_cor_written_by_era_pointplot.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_epp_per_cor_written_by_era_pointplot.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_epp_per_cor_written_by_era_pointplot.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_cor_written_by_budé_pointplot.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_cor_written_by_budé_pointplot.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_cor_written_by_budé_pointplot.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_cor_written_by_budé_pointplot.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
