54458b57c58015a7e1d707012a3a364aeeb02205
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-19 15:58:22 +0100

    Adjusted R scripts to Budé; Generated new plots

--- budé/r_scripts/no_corr_per_loc/no_corr_per_loc_writing_to_budé_pointplot.R
+++ budé/r_scripts/no_corr_per_loc/no_corr_per_loc_writing_to_budé_pointplot.R
@@ -10,12 +10,12 @@ getwd()
 setwd("../query_results/")
 
 # read data
-data<-read.csv("no_corr_per_loc/no_corr_per_loc_writing_to_era_with_geocoordinates.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+data<-read.csv("no_corr_per_loc/no_corr_per_loc_writing_to_budé_with_geocoordinates.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
 # create pointplot
-plot <- ggplot(data=data, aes(x= reorder(Location.Name.Modern, -Number.of.correspondents.who.wrote.from.this.location.letters.to.Erasmus), y=Number.of.correspondents.who.wrote.from.this.location.letters.to.Erasmus, label=Location.Name.Modern)) +
+plot <- ggplot(data=data, aes(x= reorder(Location.Name.Modern, -Number.of.correspondents.who.wrote.from.this.location.letters.to.Budé), y=Number.of.correspondents.who.wrote.from.this.location.letters.to.Budé, label=Location.Name.Modern)) +
   geom_point(stat = "identity") +
-  labs(x="Locations",y="Number of correspondents writing letters to Erasmus") +
+  labs(x="Locations",y="Number of correspondents writing letters to Budé") +
   theme_bw() +
   theme(axis.title.x=element_text(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
 plot
@@ -25,7 +25,7 @@ getwd()
 setwd("../r_plots/")
 
 # save plot in multiple formats
-ggsave("no_corr_per_loc_writing_to_era_pointplot.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_corr_per_loc_writing_to_era_pointplot.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_corr_per_loc_writing_to_era_pointplot.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_corr_per_loc_writing_to_era_pointplot.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_corr_per_loc_writing_to_budé_pointplot.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_corr_per_loc_writing_to_budé_pointplot.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_corr_per_loc_writing_to_budé_pointplot.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_corr_per_loc_writing_to_budé_pointplot.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
--- budé/r_scripts/no_corr_per_loc/no_corr_per_loc_writing_to_budé_pointplot_and_boxplot.R
+++ budé/r_scripts/no_corr_per_loc/no_corr_per_loc_writing_to_budé_pointplot_and_boxplot.R
@@ -12,23 +12,23 @@ getwd()
 setwd("../query_results/")
 
 # read data
-data<-read.csv("no_corr_per_loc/no_corr_per_loc_writing_to_era_with_geocoordinates.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+data<-read.csv("no_corr_per_loc/no_corr_per_loc_writing_to_budé_with_geocoordinates.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
 # create pointplot
-plot <- ggplot(data=data, aes(x= reorder(Location.Name.Modern, -Number.of.correspondents.who.wrote.from.this.location.letters.to.Erasmus), y=Number.of.correspondents.who.wrote.from.this.location.letters.to.Erasmus, label=Location.Name.Modern)) + 
+plot <- ggplot(data=data, aes(x= reorder(Location.Name.Modern, -Number.of.correspondents.who.wrote.from.this.location.letters.to.Budé), y=Number.of.correspondents.who.wrote.from.this.location.letters.to.Budé, label=Location.Name.Modern)) + 
   geom_point(stat = "identity") + 
-  labs(x="Locations",y="Number of correspondents writing letters to Erasmus") +
+  labs(x="Locations",y="Number of correspondents writing letters to Budé") +
   theme_bw() +
   theme(axis.title.x=element_text(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
 plot
 
 # create boxplot
-plot2 <- ggplot(data, aes(x= ' ', y = Number.of.correspondents.who.wrote.from.this.location.letters.to.Erasmus)) +
+plot2 <- ggplot(data, aes(x= ' ', y = Number.of.correspondents.who.wrote.from.this.location.letters.to.Budé)) +
   geom_boxplot(outlier.size=2, notch = FALSE) +
-  geom_text_repel(label=ifelse(data$Number.of.correspondents.who.wrote.from.this.location.letters.to.Erasmus>6,as.character(data$Location.Name.Modern),'')) +
+  geom_text_repel(label=ifelse(data$Number.of.correspondents.who.wrote.from.this.location.letters.to.Budé>1,as.character(data$Location.Name.Modern),'')) +
   theme_bw() +
   theme(axis.title.x=element_blank()) +
-  labs(y = "Number of correspondents writing letters to Erasmus")
+  labs(y = "Number of correspondents writing letters to Budé")
 plot2
 
 # arrange plots
@@ -40,7 +40,7 @@ getwd()
 setwd("../r_plots/")
 
 # save plot in multiple formats
-ggsave("no_corr_per_loc_writing_to_era_pointplot_and_boxplot.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_corr_per_loc_writing_to_era_pointplot_and_boxplot.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_corr_per_loc_writing_to_era_pointplot_and_boxplot.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_corr_per_loc_writing_to_era_pointplot_and_boxplot.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_corr_per_loc_writing_to_budé_pointplot_and_boxplot.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_corr_per_loc_writing_to_budé_pointplot_and_boxplot.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_corr_per_loc_writing_to_budé_pointplot_and_boxplot.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_corr_per_loc_writing_to_budé_pointplot_and_boxplot.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
