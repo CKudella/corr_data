810a6bcb632daef80920f54f861475b1302a09f8
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-20 10:51:09 +0100

    Added new SQL queries

---
+++ pirckheimer/sql_queries/no_corr_per_year/avg_no_epp_per_loc_year_written_to_pirck.sql
@@ -0,0 +1,28 @@
+SELECT Y.locations_name_modern AS 'Location Name',
+       X.locations_lat AS 'Latitude',
+       X.locations_lng AS 'Longitude',
+       X.count AS 'Number of years letters are written from this location to Pirckheimer',
+       Y.count AS 'Number of letters written from this location to Pirckheimer',
+       Y.COUNT/X.COUNT AS 'Average Number of Letters written from this location to Pirckheimer per year'
+FROM
+  (SELECT locations_name_modern,
+          locations_lat,
+          locations_lng,
+          COUNT(DISTINCT send_date_year1) AS COUNT
+   FROM letters
+   JOIN wpirck_cdb_v1.locations ON locations.locations_id = letters.source_loc_id
+   WHERE letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+     AND recipient_id = 'pirckheimer_willibald_viaf_27173507'
+     AND source_loc_id NOT LIKE 'unknown%'
+   GROUP BY source_loc_id
+   ORDER BY COUNT DESC) AS X
+INNER JOIN
+  (SELECT locations.locations_name_modern,
+          COUNT(letters.target_loc_id) AS COUNT
+   FROM letters
+   JOIN locations ON locations.locations_id = letters.source_loc_id
+   WHERE letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+     AND recipient_id = 'pirckheimer_willibald_viaf_27173507'
+     AND target_loc_id NOT LIKE 'unknown%'
+   GROUP BY source_loc_id
+   ORDER BY COUNT DESC) AS Y ON X.locations_name_modern = Y.locations_name_modern
