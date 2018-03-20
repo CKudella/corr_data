dbdfbde2ad17e090f66bba29c906340ee087f260
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-20 15:30:25 +0100

    Added new SQL queries; Added new query results

---
+++ pirckheimer/sql_queries/no_epp_per_loc/no_epp_per_loc_sent_by_pirck_to_outliers.sql
@@ -0,0 +1,26 @@
+SELECT B.locations_name_modern,
+       A.send_date_year1 AS YEAR,
+       COUNT(*) AS COUNT
+FROM letters AS A,
+     locations AS B
+WHERE A.target_loc_id = B.locations_id
+  AND A.sender_id = 'pirckheimer_willibald_viaf_27173507'
+  AND A.letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+  AND A.target_loc_id NOT LIKE 'unknown%'
+  AND A.target_loc_id IN
+    (SELECT C.target_loc_id
+     FROM
+       (SELECT DISTINCT D.target_loc_id,
+                        COUNT(D.target_loc_id) AS NoEppFromPirck
+        FROM letters AS D
+        JOIN locations AS E ON E.locations_id = D.target_loc_id
+        WHERE D.letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+          AND D.sender_id = 'pirckheimer_willibald_viaf_27173507'
+          AND D.target_loc_id NOT LIKE 'unknown%'
+        GROUP BY D.target_loc_id
+        ORDER BY COUNT(D.target_loc_id) DESC) AS C
+     WHERE C.NoEppFromPirck > 11)
+GROUP BY A.target_loc_id,
+         A.send_date_year1
+ORDER BY A.target_loc_id,
+         A.send_date_year1 ASC
---
+++ pirckheimer/sql_queries/no_epp_per_loc/no_epp_per_loc_sent_to_pirck_outliers.sql
@@ -0,0 +1,26 @@
+SELECT B.locations_name_modern,
+       A.send_date_year1 AS YEAR,
+       COUNT(*) AS COUNT
+FROM letters AS A,
+     locations AS B
+WHERE A.source_loc_id = B.locations_id
+  AND A.recipient_id = 'pirckheimer_willibald_viaf_27173507'
+  AND A.letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+  AND A.source_loc_id NOT LIKE 'unknown%'
+  AND A.source_loc_id IN
+    (SELECT C.source_loc_id
+     FROM
+       (SELECT DISTINCT D.source_loc_id,
+                        COUNT(D.source_loc_id) AS NoEppToPirck
+        FROM letters AS D
+        JOIN locations AS E ON E.locations_id = D.source_loc_id
+        WHERE D.letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+          AND D.recipient_id = 'pirckheimer_willibald_viaf_27173507'
+          AND D.source_loc_id NOT LIKE 'unknown%'
+        GROUP BY D.source_loc_id
+        ORDER BY COUNT(D.source_loc_id) DESC) AS C
+     WHERE C.NoEppToPirck > 8.5)
+GROUP BY A.source_loc_id,
+         A.send_date_year1
+ORDER BY A.source_loc_id,
+         A.send_date_year1 ASC
