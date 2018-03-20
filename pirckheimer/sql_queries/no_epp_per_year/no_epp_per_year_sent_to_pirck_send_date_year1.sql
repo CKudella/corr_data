445985ea8f4ada380d287348319dbe5289bd1ed6
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-20 11:09:10 +0100

    Adjusted labels in SQL queries

--- pirckheimer/sql_queries/no_epp_per_year/no_epp_per_year_sent_to_pirck_send_date_year1.sql
+++ pirckheimer/sql_queries/no_epp_per_year/no_epp_per_year_sent_to_pirck_send_date_year1.sql
@@ -1,5 +1,5 @@
 SELECT send_date_year1 AS YEAR,
-       COUNT(*) AS 'Number of letters sent to Pirckheimer per year'
+       COUNT(*) AS 'Number of letters sent to Pirckheimer this year'
 FROM letters
 WHERE letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
   AND recipient_id = 'pirckheimer_willibald_viaf_27173507'
--- pirckheimer/sql_queries/no_epp_per_year/no_epp_per_year_with_non_inferred_send_date_sent_to_pirck.sql
+++ pirckheimer/sql_queries/no_epp_per_year/no_epp_per_year_with_non_inferred_send_date_sent_to_pirck.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT send_date_year1 AS 'Year',
-                COUNT(letters_id) AS 'Number of letters with non-inferred send date sent to Pirckheimer'
+                COUNT(letters_id) AS 'Number of letters with non-inferred send date sent to Pirckheimer this year'
 FROM letters
 WHERE letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
   AND send_date_inferred = '0'
