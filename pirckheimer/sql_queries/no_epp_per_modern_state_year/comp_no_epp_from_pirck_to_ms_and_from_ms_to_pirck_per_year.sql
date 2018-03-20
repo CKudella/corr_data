b643324deea3f96e81fd6d2c327e8375b7c89cfc
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-20 10:59:04 +0100

    Added new SQL query

---
+++ pirckheimer/sql_queries/no_epp_per_modern_state_year/comp_no_epp_from_pirck_to_ms_and_from_ms_to_pirck_per_year.sql
@@ -0,0 +1,43 @@
+SELECT A.ModernState,
+       A.Year,
+       B.NoEppSentFromPirck,
+       C.NoEppSentToPirck
+FROM
+  (SELECT DISTINCT AA.locations_modern_state AS ModernState,
+                   AL.send_date_year1 AS YEAR
+   FROM locations AS AA,
+        letters AS AL
+   WHERE AA.locations_id IN
+       (SELECT DISTINCT AB.source_loc_id
+        FROM letters AS AB)
+     OR AA.locations_id IN
+       (SELECT DISTINCT AC.target_loc_id
+        FROM letters AS AC)
+   GROUP BY AL.send_date_year1,
+            AA.locations_modern_state) AS A
+LEFT OUTER JOIN
+  (SELECT DISTINCT locations.locations_modern_state AS ModernState,
+                   COUNT(*) AS NoEppSentFromPirck,
+                   send_date_year1
+   FROM letters,
+        locations
+   WHERE locations.locations_id = letters.target_loc_id
+     AND letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+     AND sender_id = 'pirckheimer_willibald_viaf_27173507'
+   GROUP BY locations_modern_state,
+            send_date_year1
+   ORDER BY send_date_year1 ASC) AS B ON B.ModernState = A.ModernState
+AND B.send_date_year1 = A.Year
+LEFT OUTER JOIN
+  (SELECT DISTINCT locations.locations_modern_state AS ModernState,
+                   COUNT(*) AS NoEppSentToPirck,
+                   send_date_year1
+   FROM letters,
+        locations
+   WHERE locations.locations_id = letters.source_loc_id
+     AND letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+     AND recipient_id = 'pirckheimer_willibald_viaf_27173507'
+   GROUP BY locations_modern_state,
+            send_date_year1
+   ORDER BY send_date_year1 ASC) AS C ON C.ModernState = A.ModernState
+AND C.send_date_year1 = A.Year
