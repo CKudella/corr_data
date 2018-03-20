5b754b564b5412a8774a847c75b7ab2e63c6ffd3
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-20 09:50:01 +0100

    Added new SQL query

---
+++ pirckheimer/sql_queries/corpus_overview/no_locs_epp_written_by_pirck_at.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(DISTINCT letters.source_loc_id) AS 'Total number of locations from which letters have been sent by Pirckheimer'
+FROM letters
+WHERE letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+  AND sender_id = 'pirckheimer_willibald_viaf_27173507'
+  AND source_loc_id NOT LIKE 'unknown%'
