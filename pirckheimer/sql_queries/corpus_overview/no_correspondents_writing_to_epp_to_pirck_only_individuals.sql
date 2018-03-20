ed0293657501398bc97cc306e94e5b0274fabe6e
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-20 09:44:58 +0100

    Updated SQL query

--- pirckheimer/sql_queries/corpus_overview/no_correspondents_writing_to_epp_to_pirck_only_individuals.sql
+++ pirckheimer/sql_queries/corpus_overview/no_correspondents_writing_to_epp_to_pirck_only_individuals.sql
@@ -2,4 +2,5 @@ SELECT COUNT(DISTINCT sender_id) AS 'Total number correspondents (type: individu
 FROM letters,
      correspondents
 WHERE recipient_id = 'pirckheimer_willibald_viaf_27173507'
+  AND sender_id != 'unnamed_person_viaf_not_applicable'
   AND correspondents.type = 'individual'
