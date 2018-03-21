ad2be55b41e2bcdc552196b68b2870fbb86ec82e
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-21 15:26:20 +0100

    Query fix

--- intersections_pirckheimer_erasmus/sql_queries/intersection_overview/id_and_names_of_mut_corr_era_pirck.sql
+++ intersections_pirckheimer_erasmus/sql_queries/intersection_overview/id_and_names_of_mut_corr_era_pirck.sql
@@ -1,11 +1,13 @@
-SELECT E.correspondents_id,
-       E.name_in_edition
-FROM wpirck_cdb_v1.correspondents AS P,
-     era_cdb_v3.correspondents AS E
-WHERE P.correspondents_id = E.correspondents_id
-  AND P.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
-                                  'erasmus_desiderius_viaf_95982394',
-                                  'pirckheimer_willibald_viaf_27173507')
-  AND E.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
-                                  'erasmus_desiderius_viaf_95982394',
-                                  'pirckheimer_willibald_viaf_27173507')
+SELECT X.correspondents_id, X.name_in_edition
+FROM era_cdb_v3.correspondents AS X
+WHERE X.correspondents_id IN
+    (SELECT P.correspondents_id
+     FROM wpirck_cdb_v1.correspondents AS P,
+          era_cdb_v3.correspondents AS E
+     WHERE P.correspondents_id = E.correspondents_id
+       AND P.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
+                                       'erasmus_desiderius_viaf_95982394',
+                                       'pirckheimer_willibald_viaf_27173507')
+       AND E.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
+                                       'erasmus_desiderius_viaf_95982394',
+                                       'pirckheimer_willibald_viaf_27173507'))
--- intersections_pirckheimer_erasmus/sql_queries/intersection_overview/names_of_mut_corr_era_pirck.sql
+++ intersections_pirckheimer_erasmus/sql_queries/intersection_overview/names_of_mut_corr_era_pirck.sql
@@ -1,10 +1,13 @@
-SELECT E.name_in_edition 'Names of mututual correspondents of Pirckheimer and Erasmus'
-FROM wpirck_cdb_v1.correspondents AS P,
-     era_cdb_v3.correspondents AS E
-WHERE P.correspondents_id = E.correspondents_id
-  AND P.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
-                                  'erasmus_desiderius_viaf_95982394',
-                                  'pirckheimer_willibald_viaf_27173507')
-  AND E.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
-                                  'erasmus_desiderius_viaf_95982394',
-                                  'pirckheimer_willibald_viaf_27173507')
+SELECT X.name_in_edition
+FROM era_cdb_v3.correspondents AS X
+WHERE X.correspondents_id IN
+    (SELECT P.correspondents_id
+     FROM wpirck_cdb_v1.correspondents AS P,
+          era_cdb_v3.correspondents AS E
+     WHERE P.correspondents_id = E.correspondents_id
+       AND P.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
+                                       'erasmus_desiderius_viaf_95982394',
+                                       'pirckheimer_willibald_viaf_27173507')
+       AND E.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
+                                       'erasmus_desiderius_viaf_95982394',
+                                       'pirckheimer_willibald_viaf_27173507'))
