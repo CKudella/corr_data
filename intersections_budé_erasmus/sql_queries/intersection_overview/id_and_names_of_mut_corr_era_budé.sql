ab5a986a7040254ad5b071a8e7fa5d50df9701e7
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-21 15:43:58 +0100

    Added new query

---
+++ intersections_budé_erasmus/sql_queries/intersection_overview/id_and_names_of_mut_corr_era_budé.sql
@@ -0,0 +1,13 @@
+SELECT X.correspondents_id, X.name_in_edition
+FROM era_cdb_v3.correspondents AS X
+WHERE X.correspondents_id IN
+    (SELECT B.correspondents_id
+     FROM budé_cdb_v1.correspondents AS B,
+          era_cdb_v3.correspondents AS E
+     WHERE B.correspondents_id = E.correspondents_id
+       AND B.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
+                                       'erasmus_desiderius_viaf_95982394',
+                                       'budé_guillaume_viaf_105878228')
+       AND E.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
+                                       'erasmus_desiderius_viaf_95982394',
+                                       'budé_guillaume_viaf_105878228'))
