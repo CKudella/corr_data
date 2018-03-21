b446d53aadc4935d187877687442299ae3a8580b
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-21 15:57:29 +0100

    Query fix

--- intersections_pirckheimer_erasmus/sql_queries/no_epp_per_year_mut_corr_era_pirck/no_epp_per_year_sent_by_era_to_mut_corr_era_pirck.sql
+++ intersections_pirckheimer_erasmus/sql_queries/no_epp_per_year_mut_corr_era_pirck/no_epp_per_year_sent_by_era_to_mut_corr_era_pirck.sql
@@ -7,8 +7,8 @@ WHERE letters_id NOT LIKE '%ck2'
     (SELECT E.correspondents_id
      FROM wpirck_cdb_v1.correspondents AS P,
           era_cdb_v3.correspondents AS E
-     WHERE correspondents_id = E.correspondents_id
-       AND correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
+     WHERE P.correspondents_id = E.correspondents_id
+       AND P.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
                                        'erasmus_desiderius_viaf_95982394',
                                        'pirckheimer_willibald_viaf_27173507')
        AND E.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
