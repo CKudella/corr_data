390e817de6ecf1938e8b038fd75a91849a62d4ef
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-21 12:01:36 +0100

    Reworked SQL queries; Added SQl queries

---
+++ intersections_budé_erasmus/sql_queries/no_epp_per_loc_from_and_to_mut_corr_era_budé/no_epp_per_loc_sent_by_budé_to_mut_corr_era_budé_incl_era.sql
@@ -0,0 +1,21 @@
+SELECT locations.locations_name_modern AS 'Location Name Modern',
+       locations.locations_lat AS 'Latitude',
+       locations.locations_lng AS 'Longitude',
+       COUNT(letters.target_loc_id) AS 'Number of letters sent to this location by Budé to mutual correspondents of his and Erasmus (incl. Erasmus)'
+FROM budé_cdb_v1.letters
+JOIN budé_cdb_v1.locations ON locations.locations_id = letters.target_loc_id
+WHERE letters_id NOT LIKE '%ck2'
+  AND sender_id = 'budé_guillaume_viaf_105878228'
+  AND recipient_id IN
+    (SELECT X.correspondents_id
+     FROM era_cdb_v3.correspondents AS X
+     WHERE X.correspondents_id IN
+         (SELECT B.correspondents_id
+          FROM budé_cdb_v1.correspondents AS B,
+               era_cdb_v3.correspondents AS E
+          WHERE B.correspondents_id = E.correspondents_id
+            AND B.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
+            AND E.correspondents_id NOT IN NOT LIKE 'unnamed_person_viaf_not_applicable'
+))
+GROUP BY target_loc_id
+ORDER BY COUNT(letters.target_loc_id) DESC
---
+++ intersections_budé_erasmus/sql_queries/no_epp_per_loc_from_and_to_mut_corr_era_budé/no_epp_per_loc_sent_by_era_to_mut_corr_era_budé_excl_budé.sql
@@ -0,0 +1,23 @@
+SELECT locations.locations_name_modern AS 'Location Name Modern',
+       locations.locations_lat AS 'Latitude',
+       locations.locations_lng AS 'Longitude',
+       COUNT(letters.target_loc_id) AS 'Number of letters sent to this location by Erasmus to mutual correspondents of his and Budé (excl. Budé)'
+FROM era_cdb_v3.letters
+JOIN era_cdb_v3.locations ON locations.locations_id = letters.target_loc_id
+WHERE letters_id NOT LIKE '%ck2'
+  AND sender_id = 'erasmus_desiderius_viaf_95982394'
+  AND recipient_id IN
+    (SELECT X.correspondents_id
+     FROM era_cdb_v3.correspondents AS X
+     WHERE X.correspondents_id IN
+         (SELECT B.correspondents_id
+          FROM budé_cdb_v1.correspondents AS B,
+               era_cdb_v3.correspondents AS E
+          WHERE B.correspondents_id = E.correspondents_id
+            AND B.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
+            AND E.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
+                                            'erasmus_desiderius_viaf_95982394',
+                                            'budé_guillaume_viaf_105878228
+')))
+GROUP BY target_loc_id
+ORDER BY COUNT(letters.target_loc_id) DESC
