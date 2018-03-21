42e7baf6cfb17eeacc499b2d6e5b2fec18bcef0b
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-21 12:43:50 +0100

    Beautified code; Label fixes

--- intersections_budé_erasmus/sql_queries/no_epp_per_year_mut_corr_era_budé/no_epp_per_year_sent_to_era_by_mut_corr_era_budé.sql
+++ intersections_budé_erasmus/sql_queries/no_epp_per_year_mut_corr_era_budé/no_epp_per_year_sent_to_era_by_mut_corr_era_budé.sql
@@ -1,1 +1,17 @@
-SELECT send_date_year1, COUNT(*) AS 'Total number of letters sent to Erasmus this year to mutual correspondents of his and Budé' from era_cdb_v3.letters WHERE letters_id NOT LIKE '%ck2' AND recipient_id = 'erasmus_desiderius_viaf_95982394' AND sender_id IN (Select E.correspondents_id from budé_cdb_v1.correspondents AS B, era_cdb_v3.correspondents AS E WHERE B.correspondents_id = E.correspondents_id AND B.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable', 'erasmus_desiderius_viaf_95982394', 'budé_guillaume_viaf_105878228') AND E.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable', 'erasmus_desiderius_viaf_95982394', 'budé_guillaume_viaf_105878228')) GROUP BY send_date_year1
+SELECT send_date_year1,
+       COUNT(*) AS 'Total number of letters sent to Erasmus this year to mutual correspondents of his and Budé (excl. Budé)'
+FROM era_cdb_v3.letters
+WHERE letters_id NOT LIKE '%ck2'
+  AND recipient_id = 'erasmus_desiderius_viaf_95982394'
+  AND sender_id IN
+    (SELECT E.correspondents_id
+     FROM budé_cdb_v1.correspondents AS B,
+          era_cdb_v3.correspondents AS E
+     WHERE B.correspondents_id = E.correspondents_id
+       AND B.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
+                                       'erasmus_desiderius_viaf_95982394',
+                                       'budé_guillaume_viaf_105878228')
+       AND E.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
+                                       'erasmus_desiderius_viaf_95982394',
+                                       'budé_guillaume_viaf_105878228'))
+GROUP BY send_date_year1
