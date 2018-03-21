f136c5e4e1841af72eaa880ad88b779c47dbc56c
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-21 11:54:54 +0100

    Reworked SQL queries; Added SQl queries

---
+++ intersections_pirckheimer_erasmus/sql_queries/no_epp_per_loc_from_and_to_mut_corr_era_pirck/no_epp_per_loc_sent_by_era_to_mut_corr_era_pirck_incl_pirck.sql
@@ -0,0 +1,21 @@
+SELECT locations.locations_name_modern AS 'Location Name Modern',
+       locations.locations_lat AS 'Latitude',
+       locations.locations_lng AS 'Longitude',
+       COUNT(letters.target_loc_id) AS 'Number of letters sent to this location by Erasmus to mutual correspondents of his and Pirckheimer (incl. Pirckheimer)'
+FROM era_cdb_v3.letters
+JOIN era_cdb_v3.locations ON locations.locations_id = letters.target_loc_id
+WHERE letters_id NOT LIKE '%ck2'
+  AND sender_id = 'erasmus_desiderius_viaf_95982394'
+  AND recipient_id IN
+    (SELECT X.correspondents_id
+     FROM era_cdb_v3.correspondents AS X
+     WHERE X.correspondents_id IN
+         (SELECT P.correspondents_id
+          FROM wpirck_cdb_v1.correspondents AS P,
+               era_cdb_v3.correspondents AS E
+          WHERE P.correspondents_id = E.correspondents_id
+            AND P.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
+            AND E.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
+))
+GROUP BY target_loc_id
+ORDER BY COUNT(letters.target_loc_id) DESC
---
+++ intersections_pirckheimer_erasmus/sql_queries/no_epp_per_loc_from_and_to_mut_corr_era_pirck/no_epp_per_loc_sent_to_pirck_from_mut_corr_era_pirck_incl_era.sql
@@ -0,0 +1,21 @@
+SELECT locations.locations_name_modern AS 'Location Name Modern',
+       locations.locations_lat AS 'Latitude',
+       locations.locations_lng AS 'Longitude',
+       COUNT(letters.source_loc_id) AS 'Number of letters sent from this location to Pirckheimer from mutual correspondents of his and Erasmus (incl. Erasmus)'
+FROM wpirck_cdb_v1.letters
+JOIN wpirck_cdb_v1.locations ON locations.locations_id = letters.source_loc_id
+WHERE letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
+  AND recipient_id = 'pirckheimer_willibald_viaf_27173507'
+  AND sender_id IN
+    (SELECT X.correspondents_id
+     FROM era_cdb_v3.correspondents AS X
+     WHERE X.correspondents_id IN
+         (SELECT P.correspondents_id
+          FROM wpirck_cdb_v1.correspondents AS P,
+               era_cdb_v3.correspondents AS E
+          WHERE P.correspondents_id = E.correspondents_id
+            AND P.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
+            AND E.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
+))
+GROUP BY source_loc_id
+ORDER BY COUNT(letters.source_loc_id) DESC
