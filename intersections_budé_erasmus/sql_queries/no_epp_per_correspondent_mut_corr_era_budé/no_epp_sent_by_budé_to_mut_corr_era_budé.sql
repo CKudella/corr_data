ae22eab74cc34af0976d6ae89f51fed2f7c41cbc
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-21 15:37:26 +0100

    Beautified code

--- intersections_budé_erasmus/sql_queries/no_epp_per_correspondent_mut_corr_era_budé/no_epp_sent_by_budé_to_mut_corr_era_budé.sql
+++ intersections_budé_erasmus/sql_queries/no_epp_per_correspondent_mut_corr_era_budé/no_epp_sent_by_budé_to_mut_corr_era_budé.sql
@@ -1,1 +1,17 @@
-SELECT DISTINCT BL.recipient_id, COUNT(*) AS 'Number of letters sent by Budé per mutual correspondent' from budé_cdb_v1.letters AS BL, (SELECT * FROM era_cdb_v3.correspondents AS X WHERE X.correspondents_id IN (Select B.correspondents_id from budé_cdb_v1.correspondents AS B, era_cdb_v3.correspondents AS E WHERE B.correspondents_id = E.correspondents_id AND B.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable' AND E.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable')) AS MC WHERE BL.recipient_id = MC.correspondents_id AND sender_id = 'budé_guillaume_viaf_105878228' GROUP BY BL.recipient_id ORDER BY COUNT(*) DESC
+SELECT DISTINCT BL.recipient_id,
+                COUNT(*) AS 'Number of letters sent by Budé per mutual correspondent'
+FROM budé_cdb_v1.letters AS BL,
+
+  (SELECT *
+   FROM era_cdb_v3.correspondents AS X
+   WHERE X.correspondents_id IN
+       (SELECT B.correspondents_id
+        FROM budé_cdb_v1.correspondents AS B,
+             era_cdb_v3.correspondents AS E
+        WHERE B.correspondents_id = E.correspondents_id
+          AND B.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
+          AND E.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable')) AS MC
+WHERE BL.recipient_id = MC.correspondents_id
+  AND sender_id = 'budé_guillaume_viaf_105878228'
+GROUP BY BL.recipient_id
+ORDER BY COUNT(*) DESC
--- intersections_budé_erasmus/sql_queries/no_epp_per_correspondent_mut_corr_era_budé/no_epp_sent_to_era_from_mut_corr_era_budé.sql
+++ intersections_budé_erasmus/sql_queries/no_epp_per_correspondent_mut_corr_era_budé/no_epp_sent_to_era_from_mut_corr_era_budé.sql
@@ -1,1 +1,17 @@
-SELECT DISTINCT EL.sender_id, COUNT(*) AS 'Number of letters sent to Erasmus per mutual correspondent' from era_cdb_v3.letters AS EL, (SELECT * FROM era_cdb_v3.correspondents AS X WHERE X.correspondents_id IN (Select B.correspondents_id from budé_cdb_v1.correspondents AS B, era_cdb_v3.correspondents AS E WHERE B.correspondents_id = E.correspondents_id AND B.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable' AND E.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable')) AS MC WHERE EL.sender_id = MC.correspondents_id AND sender_id != 'erasmus_desiderius_viaf_95982394' GROUP BY EL.sender_id ORDER BY COUNT(*) DESC
+SELECT DISTINCT EL.sender_id,
+                COUNT(*) AS 'Number of letters sent to Erasmus per mutual correspondent'
+FROM era_cdb_v3.letters AS EL,
+
+  (SELECT *
+   FROM era_cdb_v3.correspondents AS X
+   WHERE X.correspondents_id IN
+       (SELECT B.correspondents_id
+        FROM budé_cdb_v1.correspondents AS B,
+             era_cdb_v3.correspondents AS E
+        WHERE B.correspondents_id = E.correspondents_id
+          AND B.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
+          AND E.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable')) AS MC
+WHERE EL.sender_id = MC.correspondents_id
+  AND sender_id != 'erasmus_desiderius_viaf_95982394'
+GROUP BY EL.sender_id
+ORDER BY COUNT(*) DESC
