e7368857de58b3773a96693177f314c8139d2d1e
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-21 09:36:26 +0100

    Label adjustment in R script; Generated new plots

--- erasmus/r_scripts/no_epp_per_modern_state_year/comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.R
+++ erasmus/r_scripts/no_epp_per_modern_state_year/comp_no_epp_from_era_to_ms_and_from_ms_to_era_per_year_facet_grid.R
@@ -21,10 +21,12 @@ plot <- ggplot(data=data_long, aes(x=Year,y=value, colour=variable)) +
   labs(x="Year",y="Number of letters") +
   scale_x_continuous(breaks = c(1484:1536)) +
   facet_grid(ModernState ~ ., space = "free" ) +
-  theme_minimal() +
+  theme_bw() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.35)) +
   theme(strip.text.y = element_text(angle = 0, hjust = 1)) +
-  theme(legend.position="bottom")
+  theme(legend.position="bottom") +
+  theme(legend.title=element_blank()) +
+  scale_colour_discrete(labels=c("Number of letters Erasmus sent to this modern state", "Number of letters sent from this modern state to Erasmus"))
 plot
 
 # change working directory
