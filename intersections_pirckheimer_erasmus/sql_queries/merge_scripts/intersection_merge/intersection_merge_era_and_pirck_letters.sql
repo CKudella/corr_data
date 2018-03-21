7044e271ef2e1f50c541e86ba22368cec208dd06
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-21 15:15:24 +0100

    Beautified code

--- intersections_pirckheimer_erasmus/sql_queries/merge_scripts/intersection_merge/intersection_merge_era_and_pirck_letters.sql
+++ intersections_pirckheimer_erasmus/sql_queries/merge_scripts/intersection_merge/intersection_merge_era_and_pirck_letters.sql
@@ -1,1 +1,28 @@
-SELECT * FROM (SELECT * from era_cdb_v3.letters UNION ALL (Select * from wpirck_cdb_v1.letters WHERE letters_id NOT LIKE '%_cwe_%')) AS L WHERE L.sender_id IN (SELECT X.correspondents_id FROM era_cdb_v3.correspondents AS X WHERE X.correspondents_id IN (Select P.correspondents_id from wpirck_cdb_v1.correspondents AS P, era_cdb_v3.correspondents AS E WHERE P.correspondents_id = E.correspondents_id AND P.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable' AND E.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable')) AND L. recipient_id IN (SELECT X.correspondents_id FROM era_cdb_v3.correspondents AS X WHERE X.correspondents_id IN (Select P.correspondents_id from wpirck_cdb_v1.correspondents AS P, era_cdb_v3.correspondents AS E WHERE P.correspondents_id = E.correspondents_id AND P.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable' AND E.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'))
+SELECT *
+FROM
+  (SELECT *
+   FROM era_cdb_v3.letters
+   UNION ALL
+     (SELECT *
+      FROM wpirck_cdb_v1.letters
+      WHERE letters_id NOT LIKE '%_cwe_%')) AS L
+WHERE L.sender_id IN
+    (SELECT X.correspondents_id
+     FROM era_cdb_v3.correspondents AS X
+     WHERE X.correspondents_id IN
+         (SELECT P.correspondents_id
+          FROM wpirck_cdb_v1.correspondents AS P,
+               era_cdb_v3.correspondents AS E
+          WHERE P.correspondents_id = E.correspondents_id
+            AND P.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
+            AND E.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'))
+  AND L. recipient_id IN
+    (SELECT X.correspondents_id
+     FROM era_cdb_v3.correspondents AS X
+     WHERE X.correspondents_id IN
+         (SELECT P.correspondents_id
+          FROM wpirck_cdb_v1.correspondents AS P,
+               era_cdb_v3.correspondents AS E
+          WHERE P.correspondents_id = E.correspondents_id
+            AND P.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
+            AND E.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'))
