a7012764008d18c08830ce00c4c7fb7a67bb7293
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-22 18:17:23 +0100

    Updated queries

--- intersections_budé_erasmus/sql_queries/no_epp_per_loc_from_and_to_mut_corr_era_budé/no_epp_per_loc_sent_to_era_from_mut_corr_era_budé_excl_budé.sql
+++ intersections_budé_erasmus/sql_queries/no_epp_per_loc_from_and_to_mut_corr_era_budé/no_epp_per_loc_sent_to_era_from_mut_corr_era_budé_excl_budé.sql
@@ -14,8 +14,8 @@ WHERE letters_id NOT LIKE '%ck2'
           FROM budé_cdb_v1.correspondents AS B,
                era_cdb_v3.correspondents AS E
           WHERE B.correspondents_id = E.correspondents_id
-            AND E.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
-            AND B.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
+            AND B.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
+            AND E.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
                                             'erasmus_desiderius_viaf_95982394',
                                             'budé_guillaume_viaf_105878228
 ')))
