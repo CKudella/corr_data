1a63de49e9cc177c96e32ff236cc8b9b93e810da
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-22 10:26:25 +0100

    Added new SQL queries

---
+++ intersections_budé_erasmus/sql_queries/no_epp_per_corr_year_mut_corr_era_budé/no_epp_year_era_budé_with_rabelais_françois_viaf_24606939.sql
@@ -0,0 +1,44 @@
+﻿
+SELECT TE.Year,
+       EtX.EPPEtX,
+       XtE.EPPXtE,
+       PtX.EPPBtX,
+       XtP.EPPXtB
+FROM
+  (SELECT DISTINCT A.send_date_year1 AS Year
+   FROM
+     (SELECT DISTINCT send_date_year1
+      FROM era_cdb_v3.letters
+      UNION ALL SELECT DISTINCT send_date_year1
+      FROM budé_cdb_v1.letters
+      ORDER BY send_date_year1) AS A) AS TE
+LEFT JOIN
+  (SELECT send_date_year1,
+          COUNT(*) AS EPPEtX
+   FROM era_cdb_v3.letters
+   WHERE recipient_id = 'rabelais_françois_viaf_24606939'
+     AND sender_id = 'erasmus_desiderius_viaf_95982394'
+   GROUP BY send_date_year1) AS EtX ON EtX.send_date_year1 = TE.Year
+LEFT JOIN
+  (SELECT send_date_year1,
+          COUNT(*) AS EPPXtE
+   FROM era_cdb_v3.letters
+   WHERE sender_id = 'rabelais_françois_viaf_24606939'
+     AND recipient_id = 'erasmus_desiderius_viaf_95982394'
+   GROUP BY send_date_year1) AS XtE ON XtE.send_date_year1 = TE.Year
+LEFT JOIN
+  (SELECT send_date_year1,
+          COUNT(*) AS EPPBtX
+   FROM budé_cdb_v1.letters
+   WHERE recipient_id = 'rabelais_françois_viaf_24606939'
+     AND sender_id = 'budé_guillaume_viaf_105878228'
+   GROUP BY send_date_year1) AS PtX ON PtX.send_date_year1 = TE.Year
+LEFT JOIN
+  (SELECT send_date_year1,
+          COUNT(*) AS EPPXtB
+   FROM budé_cdb_v1.letters
+   WHERE sender_id = 'rabelais_françois_viaf_24606939'
+     AND recipient_id = 'budé_guillaume_viaf_105878228'
+   GROUP BY send_date_year1) AS XtP ON XtP.send_date_year1 = TE.Year
+ORDER BY TE.Year ASC
+
