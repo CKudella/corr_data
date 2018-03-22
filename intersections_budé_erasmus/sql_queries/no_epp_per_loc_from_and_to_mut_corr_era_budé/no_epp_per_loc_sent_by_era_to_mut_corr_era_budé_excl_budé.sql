f76fbba9da165f722bba7f0870aa242c93d395a6
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-22 18:26:56 +0100

    query fix

--- intersections_budé_erasmus/sql_queries/no_epp_per_loc_from_and_to_mut_corr_era_budé/no_epp_per_loc_sent_by_era_to_mut_corr_era_budé_excl_budé.sql
+++ intersections_budé_erasmus/sql_queries/no_epp_per_loc_from_and_to_mut_corr_era_budé/no_epp_per_loc_sent_by_era_to_mut_corr_era_budé_excl_budé.sql
@@ -15,9 +15,7 @@ WHERE letters_id NOT LIKE '%ck2'
                era_cdb_v3.correspondents AS E
           WHERE B.correspondents_id = E.correspondents_id
             AND B.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'
-            AND E.correspondents_id NOT IN ('unnamed_person_viaf_not_applicable',
-                                            'erasmus_desiderius_viaf_95982394',
-                                            'budé_guillaume_viaf_105878228
-')))
+            AND E.correspondents_id NOT LIKE 'unnamed_person_viaf_not_applicable'))
+  AND recipient_id != 'budé_guillaume_viaf_105878228'
 GROUP BY target_loc_id
 ORDER BY COUNT(letters.target_loc_id) DESC
