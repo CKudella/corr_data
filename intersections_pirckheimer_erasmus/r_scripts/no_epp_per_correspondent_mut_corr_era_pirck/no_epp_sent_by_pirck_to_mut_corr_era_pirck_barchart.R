725b02758eed4c88f7818b135c0316d98a658566
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-23 14:56:19 +0100

    Updated R script; Generated new plots

--- intersections_pirckheimer_erasmus/r_scripts/no_epp_per_correspondent_mut_corr_era_pirck/no_epp_sent_by_pirck_to_mut_corr_era_pirck_barchart.R
+++ intersections_pirckheimer_erasmus/r_scripts/no_epp_per_correspondent_mut_corr_era_pirck/no_epp_sent_by_pirck_to_mut_corr_era_pirck_barchart.R
@@ -9,7 +9,7 @@ getwd()
 setwd("../query_results/")
 
 # read data
-data<-read.csv("no_epp_per_correspondent_mut_corr_era_pirck/no_epp_sent_by_pirckto_mut_corr_era_pirck.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+data<-read.csv("no_epp_per_correspondent_mut_corr_era_pirck/no_epp_sent_by_pirck_to_mut_corr_era_pirck.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
 # cut uncessary label parts from name_in_edition column
 data$name_in_edition <- gsub("\\b(\\W+COE+.*)", "", data$name_in_edition)
@@ -29,7 +29,7 @@ getwd()
 setwd("../r_plots/")
 
 # save plot in multiple formats
-ggsave("no_epp_sent_by_pirckto_mut_corr_era_pirck_barchart.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_epp_sent_by_pirckto_mut_corr_era_pirck_barchart.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_epp_sent_by_pirckto_mut_corr_era_pirck_barchart.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_epp_sent_by_pirckto_mut_corr_era_pirck_barchart.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_sent_by_pirck_to_mut_corr_era_pirck_barchart.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_sent_by_pirck_to_mut_corr_era_pirck_barchart.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_sent_by_pirck_to_mut_corr_era_pirck_barchart.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_sent_by_pirck_to_mut_corr_era_pirck_barchart.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
