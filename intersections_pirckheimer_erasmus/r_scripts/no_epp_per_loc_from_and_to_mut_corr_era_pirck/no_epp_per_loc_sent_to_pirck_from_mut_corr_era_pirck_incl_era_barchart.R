7d37c4caaac029011b342b4fc4b6706ba4029c6a
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-23 15:02:42 +0100

    Updated R script; Generated new plots

--- intersections_pirckheimer_erasmus/r_scripts/no_epp_per_loc_from_and_to_mut_corr_era_pirck/no_epp_per_loc_sent_to_pirck_from_mut_corr_era_pirck_incl_era_barchart.R
+++ intersections_pirckheimer_erasmus/r_scripts/no_epp_per_loc_from_and_to_mut_corr_era_pirck/no_epp_per_loc_sent_to_pirck_from_mut_corr_era_pirck_incl_era_barchart.R
@@ -8,7 +8,7 @@ getwd()
 setwd("../query_results/")
 
 # read data
-data<-read.csv("no_epp_per_loc_from_and_to_mut_corr_era_pirck/no_epp_per_loc_sent_to_pirckfrom_mut_corr_era_pirck_incl_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+data<-read.csv("no_epp_per_loc_from_and_to_mut_corr_era_pirck/no_epp_per_loc_sent_to_pirck_from_mut_corr_era_pirck_incl_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
 # create barchart
 plot <- ggplot(data, aes(x= reorder(Location.Name.Modern, -Number.of.letters.sent.from.this.location.to.Pirckheimer.from.mutual.correspondents.of.his.and.Erasmus..incl..Erasmus.),y=Number.of.letters.sent.from.this.location.to.Pirckheimer.from.mutual.correspondents.of.his.and.Erasmus..incl..Erasmus.)) +
@@ -24,7 +24,7 @@ getwd()
 setwd("../r_plots/")
 
 # save plot in multiple formats
-ggsave("no_epp_per_loc_sent_to_pirckfrom_mut_corr_era_pirck_incl_era_barchart.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_epp_per_loc_sent_to_pirckfrom_mut_corr_era_pirck_incl_era_barchart.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_epp_per_loc_sent_to_pirckfrom_mut_corr_era_pirck_incl_era_barchart.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
-ggsave("no_epp_per_loc_sent_to_pirckfrom_mut_corr_era_pirck_incl_era_barchart.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_loc_sent_to_pirck_from_mut_corr_era_pirck_incl_era_barchart.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_loc_sent_to_pirck_from_mut_corr_era_pirck_incl_era_barchart.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_loc_sent_to_pirck_from_mut_corr_era_pirck_incl_era_barchart.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("no_epp_per_loc_sent_to_pirck_from_mut_corr_era_pirck_incl_era_barchart.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
