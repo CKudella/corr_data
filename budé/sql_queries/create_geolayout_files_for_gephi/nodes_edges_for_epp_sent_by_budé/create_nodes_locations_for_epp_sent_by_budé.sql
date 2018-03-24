64a5241c30f839ca34c731ef6e9586612f6fca74
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-24 19:50:52 +0100

    Query fix

--- budé/sql_queries/create_geolayout_files_for_gephi/nodes_edges_for_epp_sent_by_budé/create_nodes_locations_for_epp_sent_by_budé.sql
+++ budé/sql_queries/create_geolayout_files_for_gephi/nodes_edges_for_epp_sent_by_budé/create_nodes_locations_for_epp_sent_by_budé.sql
@@ -1,5 +1,5 @@
 SELECT locations_id AS 'Id',
-       locations_name_modern,
+       locations_name_modern AS 'Label',
        locations_modern_state,
        locations_modern_province,
        locations_lat,
@@ -9,10 +9,12 @@ WHERE locations_id IN
     (SELECT DISTINCT source_loc_id
      FROM letters
      WHERE sender_id = 'budé_guillaume_viaf_105878228'
-       AND source_loc_id NOT LIKE 'unknown%')
+       AND source_loc_id NOT LIKE 'unknown%'
+       AND target_loc_id NOT LIKE 'unknown%')
   OR locations_id IN
     (SELECT DISTINCT target_loc_id
      FROM letters
      WHERE sender_id = 'budé_guillaume_viaf_105878228'
+       AND source_loc_id NOT LIKE 'unknown%'
        AND target_loc_id NOT LIKE 'unknown%')
 GROUP BY locations_id
