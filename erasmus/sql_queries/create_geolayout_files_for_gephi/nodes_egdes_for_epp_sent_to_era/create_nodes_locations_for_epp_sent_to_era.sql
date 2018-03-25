aeefe8e16b04d71beb0cab05699f69602c96472b
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-25 08:39:05 +0200

    Query fix

--- erasmus/sql_queries/create_geolayout_files_for_gephi/nodes_egdes_for_epp_sent_to_era/create_nodes_locations_for_epp_sent_to_era.sql
+++ erasmus/sql_queries/create_geolayout_files_for_gephi/nodes_egdes_for_epp_sent_to_era/create_nodes_locations_for_epp_sent_to_era.sql
@@ -9,10 +9,14 @@ WHERE locations_id IN
     (SELECT DISTINCT source_loc_id
      FROM letters
      WHERE recipient_id = 'erasmus_desiderius_viaf_95982394'
-       AND source_loc_id NOT LIKE 'unknown%')
+       AND letters_id NOT LIKE '%ck2'
+       AND source_loc_id NOT LIKE 'unknown%'
+       AND target_loc_id NOT LIKE 'unknown%')
   OR locations_id IN
     (SELECT DISTINCT target_loc_id
      FROM letters
      WHERE recipient_id = 'erasmus_desiderius_viaf_95982394'
+       AND letters_id NOT LIKE '%ck2'
+       AND source_loc_id NOT LIKE 'unknown%'
        AND target_loc_id NOT LIKE 'unknown%')
 GROUP BY locations_id
