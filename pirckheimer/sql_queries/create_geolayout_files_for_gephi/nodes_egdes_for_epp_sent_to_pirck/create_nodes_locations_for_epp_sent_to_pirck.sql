89a05aa5c30bee5f89c4c7193bcbd1e00da42160
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-25 08:50:18 +0200

    Query fix

--- pirckheimer/sql_queries/create_geolayout_files_for_gephi/nodes_egdes_for_epp_sent_to_pirck/create_nodes_locations_for_epp_sent_to_pirck.sql
+++ pirckheimer/sql_queries/create_geolayout_files_for_gephi/nodes_egdes_for_epp_sent_to_pirck/create_nodes_locations_for_epp_sent_to_pirck.sql
@@ -1,5 +1,5 @@
 SELECT locations_id AS 'Id',
-       locations_name_modern,
+       locations_name_modern AS 'Label',
        locations_modern_state,
        locations_modern_province,
        locations_lat,
@@ -9,12 +9,14 @@ WHERE locations_id IN
     (SELECT DISTINCT source_loc_id
      FROM letters
      WHERE recipient_id = 'pirckheimer_willibald_viaf_27173507'
+       AND letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
        AND source_loc_id NOT LIKE 'unknown%'
        AND target_loc_id NOT LIKE 'unknown%')
   OR locations_id IN
     (SELECT DISTINCT target_loc_id
      FROM letters
      WHERE recipient_id = 'pirckheimer_willibald_viaf_27173507'
+       AND letters_id NOT REGEXP '[0-7]ck2|ck3|ck4|ck5|ck6|ck7|ck8]'
        AND source_loc_id NOT LIKE 'unknown%'
        AND target_loc_id NOT LIKE 'unknown%')
 GROUP BY locations_id
