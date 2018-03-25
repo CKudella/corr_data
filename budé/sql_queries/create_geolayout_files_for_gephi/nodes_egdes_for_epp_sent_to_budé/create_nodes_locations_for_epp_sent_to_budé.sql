3de2c967e2ee3c681b03f9112abc2b0ed797107e
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-25 08:43:21 +0200

    Query fix

--- budé/sql_queries/create_geolayout_files_for_gephi/nodes_egdes_for_epp_sent_to_budé/create_nodes_locations_for_epp_sent_to_budé.sql
+++ budé/sql_queries/create_geolayout_files_for_gephi/nodes_egdes_for_epp_sent_to_budé/create_nodes_locations_for_epp_sent_to_budé.sql
@@ -9,12 +9,14 @@ WHERE locations_id IN
     (SELECT DISTINCT source_loc_id
      FROM letters
      WHERE recipient_id = 'budé_guillaume_viaf_105878228'
+       AND letters_id NOT LIKE '%ck2'
        AND source_loc_id NOT LIKE 'unknown%'
        AND target_loc_id NOT LIKE 'unknown%')
   OR locations_id IN
     (SELECT DISTINCT target_loc_id
      FROM letters
      WHERE recipient_id = 'budé_guillaume_viaf_105878228'
+       AND letters_id NOT LIKE '%ck2'
        AND source_loc_id NOT LIKE 'unknown%'
        AND target_loc_id NOT LIKE 'unknown%')
 GROUP BY locations_id
