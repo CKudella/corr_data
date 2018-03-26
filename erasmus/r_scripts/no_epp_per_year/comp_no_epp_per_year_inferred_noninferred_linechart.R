e262c36738760c07a07ae9cc555266afc9c95cd9
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-26 20:48:36 +0200

    Updated R script; Generated updated plots

--- erasmus/r_scripts/no_epp_per_year/comp_no_epp_per_year_inferred_noninferred_linechart.R
+++ erasmus/r_scripts/no_epp_per_year/comp_no_epp_per_year_inferred_noninferred_linechart.R
@@ -27,7 +27,8 @@ data_long <- melt(data3, id.vars= c("Year"))
 # create linechart
 plot <- ggplot(data=data_long, aes(x= Year, y=value, colour=variable)) +
   geom_line(stat = "identity", size=0.9) +
-  geom_point(shape=1) + labs(x="Year",y="Number of letters") +
+  geom_point(shape=1, fill="white", stroke=1.25) +
+  labs(x="Year",y="Number of letters") +
   scale_x_continuous(breaks = c(1484:1536)) +
   scale_y_continuous(breaks = seq(0,160,10)) +
   theme_bw() +
