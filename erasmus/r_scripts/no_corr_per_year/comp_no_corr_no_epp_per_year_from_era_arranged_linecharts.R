6790c61530e58178647612ca7ef1f57d26587d4f
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-26 20:36:46 +0200

    Updated R script; Generated updated plots

--- erasmus/r_scripts/no_corr_per_year/comp_no_corr_no_epp_per_year_from_era_arranged_linecharts.R
+++ erasmus/r_scripts/no_corr_per_year/comp_no_corr_no_epp_per_year_from_era_arranged_linecharts.R
@@ -14,23 +14,32 @@ setwd("../query_results/")
 # read data
 data<-read.csv("no_corr_per_year/comp_no_corr_no_epp_per_year_from_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
+# create data frame for years 1484-1536
+data2 <- data.frame(matrix(ncol = 1, nrow = 53))
+x <- c("Year")
+colnames(data2) <- x
+data2$Year <- c(1484:1536)
+
+# merge dataframes
+data3 <- merge(x = data2, y = data, by = "Year", all.x = TRUE)
+
 # create linechart for number of correspondents
-plot <- ggplot(data=data, aes(x= Year, y=Number.of.correspondents.receiving.letters.from.Erasmus.this.year)) +
+plot <- ggplot(data=data3, aes(x= Year, y=Number.of.correspondents.receiving.letters.from.Erasmus.this.year)) +
   geom_line(stat = "identity", size=0.9) +
   geom_point(shape=1) + labs(x="Year",y="Number of correspondents") +
   scale_x_continuous(breaks = c(1484:1536)) +
-  scale_y_continuous(breaks = seq(0,70,10)) +
+  scale_y_continuous(breaks = seq(0,90,10)) +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.35)) +
   theme(legend.position="bottom")
 plot
 
 # create linechart for number of letters
-plot2 <- ggplot(data=data, aes(x= Year, y=Number.of.letters.sent.from.Erasmus.this.year)) +
+plot2 <- ggplot(data=data3, aes(x= Year, y=Number.of.letters.sent.from.Erasmus.this.year)) +
   geom_line(stat = "identity", size=0.9) +
   geom_point(shape=1) + labs(x="Year",y="Number of letters") +
   scale_x_continuous(breaks = c(1484:1536)) +
-  scale_y_continuous(breaks = seq(0,100,10)) +
+  scale_y_continuous(breaks = seq(0,150,10)) +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.35)) +
   theme(legend.position="bottom")
