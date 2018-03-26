a39e16589efa2488f008d0ed16d6c3e9e886f9d8
Christoph Kudella <Christoph.Kudella@googlemail.com>
2018-03-26 21:23:29 +0200

    Updated R script; Generated updated plots

--- budé/r_scripts/no_epp_per_year/comp_no_epp_per_year_sent_by_to_budé_linechart.R
+++ budé/r_scripts/no_epp_per_year/comp_no_epp_per_year_sent_by_to_budé_linechart.R
@@ -12,13 +12,23 @@ setwd("../query_results/")
 # read data
 data<-read.csv("no_epp_per_year/comp_no_epp_per_year_sent_by_to_budé.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
+# create data frame for years 1484-1540
+data2 <- data.frame(matrix(ncol = 1, nrow = 57))
+x <- c("send_date_year1")
+colnames(data2) <- x
+data2$send_date_year1 <- c(1484:1540)
+
+# merge dataframes
+data3 <- merge(x = data2, y = data, by = "send_date_year1", all.x = TRUE)
+
 # apply melt for wide to long
-data_long <- melt(data, id.vars= c("send_date_year1"))
+data_long <- melt(data3, id.vars= c("send_date_year1"))
 
 # create linechart
 plot <- ggplot(data=data_long, aes(x= send_date_year1, y=value, colour=variable)) +
   geom_line(stat = "identity", size=0.9) +
-  geom_point(shape=1) + labs(x="Year",y="Number of letters") +
+  geom_point(shape=1, fill="white", stroke=1.25) +
+  labs(x="Year",y="Number of letters") +
   scale_x_continuous(breaks = c(1484:1540)) +
   scale_y_continuous(breaks = seq(0,50,5)) +
   theme_bw() +
