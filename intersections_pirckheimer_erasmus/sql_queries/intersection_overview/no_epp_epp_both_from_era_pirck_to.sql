a8518fcd62306d1d91f1baded51de610fbc480a0
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-03-30 16:53:52 +0200

    Added new SQL query

---
+++ intersections_pirckheimer_erasmus/sql_queries/intersection_overview/no_epp_epp_both_from_era_pirck_to.sql
@@ -0,0 +1,17 @@
+SELECT COUNT(BL.locations_id) AS 'Number of locations to which letters have been written by both Budé and Erasmus'
+FROM budé_cdb_v1.locations AS BL
+WHERE BL.locations_id IN
+    (SELECT DISTINCT Blet.target_loc_id
+     FROM budé_cdb_v1.letters AS BLet
+     WHERE BLet.sender_id = 'budé_guillaume_viaf_105878228'
+       AND BLet.target_loc_id NOT LIKE 'unknown%'
+     GROUP BY Blet.target_loc_id)
+  AND BL.locations_id IN
+    (SELECT EL.locations_id
+     FROM era_cdb_v3.locations AS EL
+     WHERE EL.locations_id IN
+         (SELECT DISTINCT ELet.target_loc_id
+          FROM era_cdb_v3.letters AS ELet
+          WHERE ELet.sender_id = 'erasmus_desiderius_viaf_95982394'
+            AND ELet.target_loc_id NOT LIKE 'unknown%'
+          GROUP BY ELet.target_loc_id))
