1d2c8a252e22a75f4abf77cfc489409c046d5e56
Christoph Kudella <kudella@sub.uni-goettingen.de>
2018-08-29 15:45:34 +0200

    added missing queries

---
+++ erasmus/sql_queries/create_geolayout_files_for_gephi/nodes_edges_for_epp_from_era_to_nausea/create_nodes_epp_from_era_to_venice.sql
@@ -0,0 +1,23 @@
+SELECT locations_id AS 'Id',
+       geonames_id,
+       locations_name_modern,
+       locations_name_in_edition,
+       locations_modern_state,
+       locations_modern_province,
+       locations_lat,
+       locations_lng,
+       locations_ll_combined,
+       locations_further_annotation
+FROM locations
+WHERE locations_id IN
+    (SELECT DISTINCT source_loc_id
+     FROM letters
+     WHERE sender_id = 'erasmus_desiderius_viaf_95982394'
+       AND source_loc_id NOT LIKE 'unknown%'
+       AND recipient_id = 'nausea_friedrich_viaf_248203874')
+  OR locations_id IN
+    (SELECT DISTINCT target_loc_id
+     FROM letters
+     WHERE sender_id = 'erasmus_desiderius_viaf_95982394'
+       AND recipient_id = 'nausea_friedrich_viaf_248203874')
+GROUP BY locations_id
---
+++ erasmus/sql_queries/create_geolayout_files_for_gephi/nodes_edges_for_epp_from_era_to_venice/create_nodes_epp_from_era_to_venice.sql
@@ -0,0 +1,23 @@
+SELECT locations_id AS 'Id',
+       geonames_id,
+       locations_name_modern,
+       locations_name_in_edition,
+       locations_modern_state,
+       locations_modern_province,
+       locations_lat,
+       locations_lng,
+       locations_ll_combined,
+       locations_further_annotation
+FROM locations
+WHERE locations_id IN
+    (SELECT DISTINCT source_loc_id
+     FROM letters
+     WHERE sender_id = 'erasmus_desiderius_viaf_95982394'
+       AND source_loc_id NOT LIKE 'unknown%'
+       AND target_loc_id LIKE 'venice%')
+  OR locations_id IN
+    (SELECT DISTINCT target_loc_id
+     FROM letters
+     WHERE sender_id = 'erasmus_desiderius_viaf_95982394'
+       AND target_loc_id LIKE 'venice%')
+GROUP BY locations_id
