80a1d81a0cbaeb8273c1ea5924365cc73f94b6a0
Christoph Kudella <kudella@sub.uni-goettingen.de>
2019-04-18 14:10:41 +0200

    query fix

--- erasmus/sql_queries/no_epp_per_loc/comp_epp_per_loc_sent_to_era_from_and_by_era_to.sql
+++ erasmus/sql_queries/no_epp_per_loc/comp_epp_per_loc_sent_to_era_from_and_by_era_to.sql
@@ -1,9 +1,31 @@
-SELECT X.LocationName,
-       X.Latitude,
-       X.Longitude,
+SELECT Z.LocationName,
+       Z.Latitude,
+       Z.Longitude,
        X.NoLettersWrittenTOErasmus,
        Y.NoLettersWrittenBYErasmusTO
-FROM
+FROM (
+        (SELECT DISTINCT ZB.locations_name_modern AS LocationName,
+                         ZB.locations_lat AS Latitude,
+                         ZB.locations_lng AS Longitude
+         FROM letters AS ZA,
+              locations AS ZB
+         WHERE ZB.locations_id = ZA.target_loc_id
+           AND ZA.letters_id NOT LIKE '%ck2'
+           AND ZA.sender_id = 'erasmus_desiderius_viaf_95982394'
+         GROUP BY ZB.locations_name_modern
+         ORDER BY ZB.locations_name_modern DESC)
+      UNION
+        (SELECT DISTINCT ZB.locations_name_modern AS LocationName,
+                         ZB.locations_lat AS Latitude,
+                         ZB.locations_lng AS Longitude
+         FROM letters AS ZA,
+              locations AS ZB
+         WHERE ZB.locations_id = ZA.source_loc_id
+           AND ZA.letters_id NOT LIKE '%ck2'
+           AND ZA.recipient_id = 'erasmus_desiderius_viaf_95982394'
+         GROUP BY ZB.locations_name_modern
+         ORDER BY ZB.locations_name_modern DESC)) AS Z
+LEFT OUTER JOIN
   (SELECT XB.locations_name_modern AS LocationName,
           XB.locations_lat AS 'Latitude',
           XB.locations_lng AS 'Longitude',
@@ -14,8 +36,8 @@ FROM
      AND XA.recipient_id = 'erasmus_desiderius_viaf_95982394'
      AND XA.source_loc_id NOT LIKE 'unknown%'
    GROUP BY XA.source_loc_id
-   ORDER BY COUNT(XA.source_loc_id) DESC) AS X
-INNER JOIN
+   ORDER BY COUNT(XA.source_loc_id) DESC) AS X ON X.LocationName = Z.LocationName
+LEFT OUTER JOIN
   (SELECT YB.locations_name_modern AS LocationName,
           YB.locations_lat AS 'Latitude',
           YB.locations_lng AS 'Longitude',
@@ -26,4 +48,4 @@ INNER JOIN
      AND YA.recipient_id != 'erasmus_desiderius_viaf_95982394'
      AND YA.target_loc_id NOT LIKE 'unknown%'
    GROUP BY YA.target_loc_id
-   ORDER BY COUNT(YA.target_loc_id) DESC) AS Y ON Y.LocationName = X.LocationName
+   ORDER BY COUNT(YA.target_loc_id) DESC) AS Y ON Y.LocationName = Z.LocationName
