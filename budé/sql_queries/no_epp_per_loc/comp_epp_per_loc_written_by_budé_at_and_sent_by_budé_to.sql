f04358f18c3234e569c4af4be92b00751e9fa2c4
Christoph Kudella <kudella@sub.uni-goettingen.de>
2019-04-18 15:14:05 +0200

    query fix

--- budé/sql_queries/no_epp_per_loc/comp_epp_per_loc_written_by_budé_at_and_sent_by_budé_to.sql
+++ budé/sql_queries/no_epp_per_loc/comp_epp_per_loc_written_by_budé_at_and_sent_by_budé_to.sql
@@ -1,29 +1,53 @@
-SELECT X.LocationName,
-       X.Latitude,
-       X.Longitude,
-       X.NoLettersFROMBudéTO AS 'Number of letters written by Budé to this location',
-       Y.NoLettersBYBudéAT AS 'Number of letters written by Budé at this location'
-FROM
+SELECT Z.LocationName,
+       Z.Latitude,
+       Z.Longitude,
+       X.NoLettersWrittenByBudéTO,
+       Y.NoLettersWrittenBYBudéAT
+FROM (
+        (SELECT DISTINCT ZB.locations_name_modern AS LocationName,
+                         ZB.locations_lat AS Latitude,
+                         ZB.locations_lng AS Longitude
+         FROM letters AS ZA,
+              locations AS ZB
+         WHERE ZB.locations_id = ZA.target_loc_id
+           AND ZA.letters_id NOT LIKE '%ck2'
+           AND ZA.sender_id = 'budé_guillaume_viaf_105878228'
+           AND ZA.target_loc_id NOT LIKE 'unknown%'
+         GROUP BY ZB.locations_name_modern
+         ORDER BY ZB.locations_name_modern DESC)
+      UNION
+        (SELECT DISTINCT ZB.locations_name_modern AS LocationName,
+                         ZB.locations_lat AS Latitude,
+                         ZB.locations_lng AS Longitude
+         FROM letters AS ZA,
+              locations AS ZB
+         WHERE ZB.locations_id = ZA.source_loc_id
+           AND ZA.letters_id NOT LIKE '%ck2'
+           AND ZA.sender_id = 'budé_guillaume_viaf_105878228'
+           AND ZA.source_loc_id NOT LIKE 'unknown%'
+         GROUP BY ZB.locations_name_modern
+         ORDER BY ZB.locations_name_modern DESC)) AS Z
+LEFT OUTER JOIN
   (SELECT XB.locations_name_modern AS LocationName,
           XB.locations_lat AS 'Latitude',
           XB.locations_lng AS 'Longitude',
-          COUNT(XA.target_loc_id) AS NoLettersFROMBudéTO
-   FROM budé_cdb_v1.letters AS XA
-   JOIN budé_cdb_v1.locations AS XB ON XB.locations_id = XA.source_loc_id
+          COUNT(XA.target_loc_id) AS NoLettersWrittenByBudéTO
+   FROM letters AS XA
+   JOIN locations AS XB ON XB.locations_id = XA.target_loc_id
    WHERE XA.letters_id NOT LIKE '%ck2'
-     AND XA.recipient_id = 'budé_guillaume_viaf_105878228'
+     AND XA.sender_id = 'budé_guillaume_viaf_105878228'
      AND XA.target_loc_id NOT LIKE 'unknown%'
    GROUP BY XA.target_loc_id
-   ORDER BY COUNT(XA.target_loc_id) DESC) AS X
-INNER JOIN
+   ORDER BY COUNT(XA.target_loc_id) DESC) AS X ON X.LocationName = Z.LocationName
+LEFT OUTER JOIN
   (SELECT YB.locations_name_modern AS LocationName,
           YB.locations_lat AS 'Latitude',
           YB.locations_lng AS 'Longitude',
-          COUNT(YA.source_loc_id) AS NoLettersBYBudéAT
-   FROM budé_cdb_v1.letters AS YA
-   JOIN budé_cdb_v1.locations AS YB ON YB.locations_id = YA.source_loc_id
+          COUNT(YA.source_loc_id) AS NoLettersWrittenBYBudéAT
+   FROM letters AS YA
+   JOIN locations AS YB ON YB.locations_id = YA.source_loc_id
    WHERE YA.letters_id NOT LIKE '%ck2'
      AND YA.sender_id = 'budé_guillaume_viaf_105878228'
      AND YA.source_loc_id NOT LIKE 'unknown%'
    GROUP BY YA.source_loc_id
-   ORDER BY COUNT(YA.source_loc_id) DESC) AS Y ON Y.LocationName = X.LocationName
+   ORDER BY COUNT(YA.source_loc_id) DESC) AS Y ON Y.LocationName = Z.LocationName
