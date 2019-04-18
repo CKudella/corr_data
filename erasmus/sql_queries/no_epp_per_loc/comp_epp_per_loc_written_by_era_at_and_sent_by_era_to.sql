0847a0716a629545b26fd8b75dd296a0290e2642
Christoph Kudella <kudella@sub.uni-goettingen.de>
2019-04-18 14:42:58 +0200

    query fix

--- erasmus/sql_queries/no_epp_per_loc/comp_epp_per_loc_written_by_era_at_and_sent_by_era_to.sql
+++ erasmus/sql_queries/no_epp_per_loc/comp_epp_per_loc_written_by_era_at_and_sent_by_era_to.sql
@@ -12,6 +12,7 @@ FROM (
          WHERE ZB.locations_id = ZA.target_loc_id
            AND ZA.letters_id NOT LIKE '%ck2'
            AND ZA.sender_id = 'erasmus_desiderius_viaf_95982394'
+           AND ZA.target_loc_id NOT LIKE 'unknown%'
          GROUP BY ZB.locations_name_modern
          ORDER BY ZB.locations_name_modern DESC)
       UNION
@@ -22,21 +23,22 @@ FROM (
               locations AS ZB
          WHERE ZB.locations_id = ZA.source_loc_id
            AND ZA.letters_id NOT LIKE '%ck2'
-           AND ZA.recipient_id = 'erasmus_desiderius_viaf_95982394'
+           AND ZA.sender_id = 'erasmus_desiderius_viaf_95982394'
+           AND ZA.source_loc_id NOT LIKE 'unknown%'
          GROUP BY ZB.locations_name_modern
          ORDER BY ZB.locations_name_modern DESC)) AS Z
 LEFT OUTER JOIN
   (SELECT XB.locations_name_modern AS LocationName,
           XB.locations_lat AS 'Latitude',
           XB.locations_lng AS 'Longitude',
-          COUNT(XA.source_loc_id) AS NoLettersWrittenByErasmusTO
+          COUNT(XA.target_loc_id) AS NoLettersWrittenByErasmusTO
    FROM letters AS XA
-   JOIN locations AS XB ON XB.locations_id = XA.source_loc_id
+   JOIN locations AS XB ON XB.locations_id = XA.target_loc_id
    WHERE XA.letters_id NOT LIKE '%ck2'
-     AND XA.recipient_id = 'erasmus_desiderius_viaf_95982394'
-     AND XA.source_loc_id NOT LIKE 'unknown%'
-   GROUP BY XA.source_loc_id
-   ORDER BY COUNT(XA.source_loc_id) DESC) AS X ON X.LocationName = Z.LocationName
+     AND XA.sender_id = 'erasmus_desiderius_viaf_95982394'
+     AND XA.target_loc_id NOT LIKE 'unknown%'
+   GROUP BY XA.target_loc_id
+   ORDER BY COUNT(XA.target_loc_id) DESC) AS X ON X.LocationName = Z.LocationName
 LEFT OUTER JOIN
   (SELECT YB.locations_name_modern AS LocationName,
           YB.locations_lat AS 'Latitude',
