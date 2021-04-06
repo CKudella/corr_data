a76dd78574d491b4ce820fbbf97d44b651c7339d
Christoph Kudella <Christoph.Kudella@googlemail.com>
2021-04-06 17:58:24 +0200

    removed duplicate package requirements

--- erasmus/r_scripts/no_epp_per_modern_state_year/no_epp_to_era_from_modern_state_per_year_facet_grid.R
+++ erasmus/r_scripts/no_epp_per_modern_state_year/no_epp_to_era_from_modern_state_per_year_facet_grid.R
@@ -1,25 +1,23 @@
 require(readr)
 require(ggplot2)
-library(readr)
-library(ggplot2)
 
 # set working directory
 getwd()
 setwd("../query_results/")
 
 # read data
-data<-read.csv("no_epp_per_modern_state_year/no_epp_to_era_from_modern_state_per_year.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
+data <- read.csv("no_epp_per_modern_state_year/no_epp_to_era_from_modern_state_per_year.csv", fileEncoding = "UTF-8", na.strings = c("NULL"))
 
-# # create barchart with facet grid
-plot <- ggplot(data=data, aes(x=send_date_year1,y=Number.of.letters.sent.from.this.modern.state.to.Erasmus.this.year)) +
+# create barchart with facet grid
+plot <- ggplot(data = data, aes(x = send_date_year1, y = Number.of.letters.sent.from.this.modern.state.to.Erasmus.this.year)) +
   geom_bar(stat = "identity") +
-  labs(x="Year",y="Number of letters") +
+  labs(x = "Year", y = "Number of letters") +
   scale_x_continuous(breaks = c(1484:1536)) +
-  facet_grid(Modern.State ~ ., space = "free" )+
+  facet_grid(Modern.State ~ ., space = "free") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.35)) +
   theme(strip.text.y = element_text(angle = 0, hjust = 1)) +
-  theme(legend.position="bottom")
+  theme(legend.position = "bottom")
 plot
 
 # change working directory
