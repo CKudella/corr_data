8cbc1e030d62a64db7716ced9e337ad3798e2d62
Christoph Kudella <Christoph.Kudella@googlemail.com>
2021-04-06 15:32:26 +0200

    removed duplicate package spec

--- erasmus/r_scripts/no_corr_per_modern_state/comp_no_corr_per_ms_writing_to_receiving_from_era_barchart.R
+++ erasmus/r_scripts/no_corr_per_modern_state/comp_no_corr_per_ms_writing_to_receiving_from_era_barchart.R
@@ -1,32 +1,29 @@
 require(readr)
 require(reshape2)
 require(ggplot2)
-library(readr)
-library(reshape2)
-library(ggplot2)
 
 # set working directory
 getwd()
 setwd("../query_results/")
 
 # read data
-data<-read.csv("no_corr_per_modern_state/comp_no_corr_per_ms_writing_to_receiving_from_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"), colClasses=c("Number.of.correspondents.who.received.letters.from.Erasmus"="character","Number.of.correspondents.who.wrote.letters.to.Erasmus"="character"))
+data <- read.csv("no_corr_per_modern_state/comp_no_corr_per_ms_writing_to_receiving_from_era.csv", fileEncoding = "UTF-8", na.strings = c("NULL"), colClasses = c("Number.of.correspondents.who.received.letters.from.Erasmus" = "character", "Number.of.correspondents.who.wrote.letters.to.Erasmus" = "character"))
 
 # set number columns to numeric
 data$Number.of.correspondents.who.received.letters.from.Erasmus <- as.numeric(as.character(data$Number.of.correspondents.who.received.letters.from.Erasmus))
 data$Number.of.correspondents.who.wrote.letters.to.Erasmus <- as.numeric(as.character(data$Number.of.correspondents.who.wrote.letters.to.Erasmus))
 
 # apply melt for wide to long
-data_long <- melt(data, id.vars= c("Modern.State"))
+data_long <- melt(data, id.vars = c("Modern.State"))
 
 # create barchart
-plot <- ggplot(data_long,aes(x= reorder(Modern.State,-value),y=value, fill=variable)) +
+plot <- ggplot(data_long, aes(x = reorder(Modern.State, -value), y = value, fill = variable)) +
   geom_bar(position = "dodge", stat = "identity") +
-  labs(x="Modern State",y="Number of correspondents") +
+  labs(x = "Modern State", y = "Number of correspondents") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.35)) +
-  theme(legend.position="bottom") +
-  theme(legend.title=element_blank()) +
+  theme(legend.position = "bottom") +
+  theme(legend.title = element_blank()) +
   scale_fill_grey(labels = c("Number of correspondents Erasmus wrote letters to", "Number of correspondents who wrote letters to Erasmus"))
 plot
 
