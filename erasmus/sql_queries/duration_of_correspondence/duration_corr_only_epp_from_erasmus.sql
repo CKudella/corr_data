5e39841a2fc42452a3e9e8ead7bc33ff0af5d2ec
Christoph Kudella <kudella@sub.uni-goettingen.de>
2021-04-07 15:50:15 +0200

    reworked sql queries

--- erasmus/sql_queries/duration_of_correspondence/duration_corr_only_epp_from_erasmus.sql
+++ erasmus/sql_queries/duration_of_correspondence/duration_corr_only_epp_from_erasmus.sql
@@ -1,6 +1,10 @@
 SELECT DISTINCT XA.recipient_id,
-                   MIN(XA.send_date_computable1) AS FLFE,
-                   MAX(XA.send_date_computable1) AS LLFE
-   FROM era_cdb_v3.letters AS XA
-   WHERE XA.sender_id = 'erasmus_desiderius_viaf_95982394'
-   GROUP BY XA.recipient_id
+                MIN(XA.send_date_computable1) AS FLFE,
+                MAX(XA.send_date_computable1) AS LLFE
+FROM era_cdb_v3.letters AS XA
+WHERE XA.sender_id = 'erasmus_desiderius_viaf_95982394'
+  AND XA.recipient_id NOT IN
+    (SELECT DISTINCT sender_id
+     FROM era_cdb_v3.letters
+     WHERE recipient_id = 'erasmus_desiderius_viaf_95982394')
+GROUP BY XA.recipient_id
--- erasmus/sql_queries/duration_of_correspondence/duration_corr_only_epp_to_erasmus.sql
+++ erasmus/sql_queries/duration_of_correspondence/duration_corr_only_epp_to_erasmus.sql
@@ -1,6 +1,10 @@
 SELECT DISTINCT XA.sender_id,
-                   MIN(XA.send_date_computable1) AS FLTE,
-                   MAX(XA.send_date_computable1) AS LLTE
-   FROM era_cdb_v3.letters AS XA
-   WHERE XA.recipient_id = 'erasmus_desiderius_viaf_95982394'
-   GROUP BY XA.sender_id
+                MIN(XA.send_date_computable1) AS FLTE,
+                MAX(XA.send_date_computable1) AS LLTE
+FROM era_cdb_v3.letters AS XA
+WHERE XA.recipient_id = 'erasmus_desiderius_viaf_95982394'
+  AND XA.sender_id NOT IN
+    (SELECT DISTINCT recipient_id
+     FROM era_cdb_v3.letters
+     WHERE sender_id = 'erasmus_desiderius_viaf_95982394')
+GROUP BY XA.sender_id
