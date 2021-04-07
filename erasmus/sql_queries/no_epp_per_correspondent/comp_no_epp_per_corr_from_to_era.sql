4b2641928d69da7162d7f5498e1ecc908c2d9052
Christoph Kudella <Christoph.Kudella@googlemail.com>
2021-04-07 10:23:37 +0200

    added sql script

---
+++ erasmus/sql_queries/no_epp_per_correspondent/comp_no_epp_per_corr_from_to_era.sql
@@ -0,0 +1,20 @@
+SELECT A.correspondents_id,
+       B.NoEPPfromEra,
+       C.NoEPPtoEra
+FROM
+  (SELECT DISTINCT correspondents_id
+   FROM correspondents) AS A
+LEFT JOIN
+  (SELECT DISTINCT recipient_id,
+                   COUNT(*) 'NoEPPfromEra'
+   FROM letters
+   WHERE sender_id = 'erasmus_desiderius_viaf_95982394'
+   GROUP BY recipient_id
+   ORDER BY COUNT('NoEPPfromEra') DESC) AS B ON A.correspondents_id = B.recipient_id
+LEFT JOIN
+  (SELECT DISTINCT sender_id,
+                   COUNT(*) 'NoEPPtoEra'
+   FROM letters
+   WHERE recipient_id = 'erasmus_desiderius_viaf_95982394'
+   GROUP BY sender_id
+   ORDER BY COUNT('NoEPPtoEra') DESC) AS C ON A.correspondents_id = C.sender_id"
