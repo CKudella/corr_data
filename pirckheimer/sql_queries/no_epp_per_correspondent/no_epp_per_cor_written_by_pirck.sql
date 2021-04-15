7e072737b80d952e95072dea30d2a2752bca9558
Christoph Kudella <kudella@sub.uni-goettingen.de>
2021-04-15 16:36:11 +0200

    aligned query with queries for the other corpora

--- pirckheimer/sql_queries/no_epp_per_correspondent/no_epp_per_cor_written_by_pirck.sql
+++ pirckheimer/sql_queries/no_epp_per_correspondent/no_epp_per_cor_written_by_pirck.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT recipient_id,
-                COUNT(*) 'Number of letters sent by Pirckheimer'
+                COUNT(*) 'Number of letters sent from Pirckheimer to this correspondent'
 FROM letters
 WHERE sender_id = 'pirckheimer_willibald_viaf_27173507'
 GROUP BY recipient_id
--- pirckheimer/sql_queries/no_epp_per_correspondent/no_epp_per_cor_written_to_pirck.sql
+++ pirckheimer/sql_queries/no_epp_per_correspondent/no_epp_per_cor_written_to_pirck.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT sender_id,
-                COUNT(*) 'Number of letters sent to Pirckheimer'
+                COUNT(*) 'Number of letters sent to Pirckheimer from this correspondent'
 FROM letters
 WHERE recipient_id = 'pirckheimer_willibald_viaf_27173507'
 GROUP BY sender_id
