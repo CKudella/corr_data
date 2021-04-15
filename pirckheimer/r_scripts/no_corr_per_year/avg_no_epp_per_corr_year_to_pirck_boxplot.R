f969d2ff2c64fcbc1c4a891888ef00b8d10e932b
Christoph Kudella <kudella@sub.uni-goettingen.de>
2021-04-15 15:57:30 +0200

    added new r scripts

---
+++ pirckheimer/r_scripts/no_corr_per_year/avg_no_epp_per_corr_year_to_pirck_boxplot.R
@@ -0,0 +1,39 @@
+require(readr)
+require(ggplot2)
+require(ggrepel)
+
+# set working directory
+getwd()
+setwd("../query_results/")
+
+# read data
+data <- read.csv("no_corr_per_year/avg_no_epp_per_corr_year_to_pirck.csv", fileEncoding = "UTF-8")
+
+# caculate quartiles
+quartiles <- as.numeric(quantile(data$Average.number.of.letters.sent.per.correspondent.to.Pirckheimer.this.year, probs = c(0.25, 0.5, 0.75)))
+
+# calculate IQR
+IQR <- diff(quartiles[c(1, 3)])
+
+# calculate upper whisker
+upper_whisker <- max(data$Average.number.of.letters.sent.per.correspondent.to.Pirckheimer.this.year[data$Average.number.of.letters.sent.per.correspondent.to.Pirckheimer.this.year < (quartiles[3] + 1.5 * IQR)])
+
+
+# create boxplot
+plot <- ggplot(data, aes(x = " ", y = Average.number.of.letters.sent.per.correspondent.to.Pirckheimer.this.year)) +
+  geom_boxplot(notch = FALSE) +
+  geom_text_repel(label = ifelse(data$Average.number.of.letters.sent.per.correspondent.to.Pirckheimer.this.year > upper_whisker, as.character(data$Year), "")) +
+  theme_bw() +
+  theme(axis.title.x = element_blank()) +
+  labs(y = "Average number of letters to Pirckheimer per correspondent")
+plot
+
+# change working directory
+getwd()
+setwd("../r_plots/")
+
+# save plot in multiple formats
+ggsave("avg_no_epp_per_corr_year_to_pirck_boxplot.pdf", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("avg_no_epp_per_corr_year_to_pirck_boxplot.png", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("avg_no_epp_per_corr_year_to_pirck_boxplot.eps", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
+ggsave("avg_no_epp_per_corr_year_to_pirck_boxplot.svg", plot = last_plot(), scale = 1, width = 11.7, height = 8.3, units = "in", dpi = 600, limitsize = TRUE)
