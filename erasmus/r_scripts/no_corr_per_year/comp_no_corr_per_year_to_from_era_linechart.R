49197d412aaad7d849415a9f93d982a7826cd0dd
Christoph Kudella <kudella@sub.uni-goettingen.de>
2023-08-31 16:55:12 +0200

    fixed r_script

--- erasmus/r_scripts/no_corr_per_year/comp_no_corr_per_year_to_from_era_linechart.R
+++ erasmus/r_scripts/no_corr_per_year/comp_no_corr_per_year_to_from_era_linechart.R
@@ -9,16 +9,13 @@ setwd("../query_results/")
 data<-read.csv("no_corr_per_year/comp_no_corr_per_year_to_from_era.csv", fileEncoding="UTF-8", na.strings=c("NULL"))
 
 # create data frame for years 1484-1536
-data2 <- data.frame(matrix(ncol = 1, nrow = 53))
-x <- c("Year")
-colnames(data2) <- x
-data2$Year <- c(1484:1536)
-
-# create data frame for years 1484-1536
 data2 <- tibble(Year = 1484:1536)
 
 # merge data frames
 data3 <- left_join(data2, data, by = "Year")
+
+# pivot data from wide to long format
+data_long <- data %>% pivot_longer(cols = c("Number.of.correspondents.Erasmus.writes.to.this.year", "Number.of.correspondents.writing.letters.to.Erasmus.this.year"), names_to = "variable", values_to = "value")
 
 # create linechart
 plot <- ggplot(data=data_long, aes(x= Year, y=value, colour=variable)) +
