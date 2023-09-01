7f53afe0363c3d35a7abde18e4937ccc4775a9ef
Christoph Kudella <Christoph.Kudella@googlemail.com>
2023-09-01 22:04:31 +0200

    modified queries to include correspondents' names

--- budé/sql_queries/no_epp_per_correspondent/no_epp_per_cor_written_by_budé.sql
+++ budé/sql_queries/no_epp_per_correspondent/no_epp_per_cor_written_by_budé.sql
@@ -1,6 +1,7 @@
-SELECT DISTINCT recipient_id,
-                COUNT(*) 'Number of letters sent from Budé to this correspondent'
-FROM letters
-WHERE sender_id = 'c0b89c75-45b8-4b04-bfd7-25bfe9ed040b'
-GROUP BY recipient_id
-ORDER BY COUNT(*) DESC
+SELECT DISTINCT L.recipient_id, C.name_in_edition,
+                COUNT(*) AS 'Number of letters sent from Budé to this correspondent'
+FROM letters AS L
+JOIN correspondents AS C ON L.recipient_id = C.correspondents_id
+WHERE L.sender_id = 'c0b89c75-45b8-4b04-bfd7-25bfe9ed040b'
+GROUP BY L.recipient_id, C.name_in_edition
+ORDER BY COUNT(*) DESC;
--- budé/sql_queries/no_epp_per_correspondent/no_epp_per_cor_written_to_budé.sql
+++ budé/sql_queries/no_epp_per_correspondent/no_epp_per_cor_written_to_budé.sql
@@ -1,6 +1,7 @@
-SELECT DISTINCT sender_id,
-                COUNT(*) 'Number of letters sent to Budé from this correspondent'
-FROM letters
-WHERE recipient_id = 'c0b89c75-45b8-4b04-bfd7-25bfe9ed040b'
-GROUP BY sender_id
-ORDER BY COUNT(*) DESC
+SELECT DISTINCT L.sender_id, C.name_in_edition,
+                COUNT(*) AS 'Number of letters sent to Budé from this correspondent'
+FROM letters AS L
+JOIN correspondents AS C ON L.sender_id = C.correspondents_id
+WHERE L.recipient_id = 'c0b89c75-45b8-4b04-bfd7-25bfe9ed040b'
+GROUP BY L.sender_id, C.name_in_edition
+ORDER BY COUNT(*) DESC;
