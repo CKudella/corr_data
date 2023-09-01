7f95a7f299e70bf22e43e802bba6e56ff090d058
Christoph Kudella <Christoph.Kudella@googlemail.com>
2023-09-01 23:13:21 +0200

    updated r_scripts for no_epp_per_correspondents

--- pirckheimer/r_scripts/no_epp_per_correspondent/no_epp_per_cor_written_to_pirck_boxplot.R
+++ pirckheimer/r_scripts/no_epp_per_correspondent/no_epp_per_cor_written_to_pirck_boxplot.R
@@ -1,4 +1,5 @@
 require(tidyverse)
+require(ggrepel)
 require(svglite)
 
 # set working directory
@@ -8,13 +9,25 @@ setwd("../query_results/")
 # read data
 data <- read.csv("no_epp_per_correspondent/no_epp_per_cor_written_to_pirck.csv", fileEncoding = "UTF-8", na.strings = c("NULL"))
 
-# create violin and boxplot combined
+#identify outliers
+outliers_df <- data %>%
+  mutate(
+    Q1 = quantile(Number.of.letters.sent.to.Pirckheimer.from.this.correspondent, 0.25),
+    Q3 = quantile(Number.of.letters.sent.to.Pirckheimer.from.this.correspondent, 0.75),
+    IQR = Q3 - Q1,
+    lower_bound = Q1 - 1.5 * IQR,
+    upper_bound = Q3 + 1.5 * IQR,
+    is_outlier = Number.of.letters.sent.to.Pirckheimer.from.this.correspondent < lower_bound | Number.of.letters.sent.to.Pirckheimer.from.this.correspondent > upper_bound
+  ) %>%
+  filter(is_outlier)
+
+# create box plot
 plot <- ggplot(data, aes(x = " ", y = Number.of.letters.sent.to.Pirckheimer.from.this.correspondent)) +
-  geom_violin() +
-  geom_boxplot(width = 0.1, color = "black", outlier.alpha = 0.25) +
+  geom_boxplot(width = 0.1, color = "black", outlier.alpha = 0.5) +
+  geom_text_repel(data = outliers_df, aes(label = name_in_edition), hjust = 1.2, vjust = 0, box.padding = 3, max.overlaps = Inf, size = 2.5) +
+  labs(x = "Correspondent", y = "Number of letters sent to Pirckheimer by this correspondent") +
   theme_bw() +
-  theme(axis.title.x = element_blank()) +
-  labs(y = "Number of letters written to Pirckheimer")
+  theme(axis.title.x = element_text(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
 plot
 
 # change working directory
