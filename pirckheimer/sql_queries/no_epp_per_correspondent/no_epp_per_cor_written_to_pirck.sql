ff10aefc1fa8641ce07b18c2bb9df8a9348ee4da
Christoph Kudella <Christoph.Kudella@googlemail.com>
2023-09-01 22:38:49 +0200

    modified queries to include correspondents' names

--- pirckheimer/sql_queries/no_epp_per_correspondent/no_epp_per_cor_written_to_pirck.sql
+++ pirckheimer/sql_queries/no_epp_per_correspondent/no_epp_per_cor_written_to_pirck.sql
@@ -1,6 +1,7 @@
-SELECT DISTINCT sender_id,
-                COUNT(*) 'Number of letters sent to Pirckheimer from this correspondent'
-FROM letters
-WHERE recipient_id = 'd9233b24-a98c-4279-8065-e2ab70c0d080'
-GROUP BY sender_id
-ORDER BY COUNT(*) DESC
+SELECT DISTINCT L.sender_id, C.name_in_edition,
+                COUNT(*) AS 'Number of letters sent to Pirckheimer from this correspondent'
+FROM letters AS L
+JOIN correspondents AS C ON L.sender_id = C.correspondents_id
+WHERE L.recipient_id = 'd9233b24-a98c-4279-8065-e2ab70c0d080'
+GROUP BY L.sender_id, C.name_in_edition
+ORDER BY COUNT(*) DESC;
