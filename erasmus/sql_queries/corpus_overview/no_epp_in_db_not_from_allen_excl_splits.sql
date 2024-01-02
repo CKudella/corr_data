f0f141db56b75a4f3dfcfbe9ec8566eec640680f
Christoph Kudella <Christoph.Kudella@googlemail.com>
2024-01-02 12:23:29 +0100

    minor fix

--- erasmus/sql_queries/corpus_overview/no_epp_in_db_not_from_allen_excl_splits.sql
+++ erasmus/sql_queries/corpus_overview/no_epp_in_db_not_from_allen_excl_splits.sql
@@ -1,5 +1,5 @@
-SELECT COUNT(*) AS 'Total number of letters in DB that are not part of the ALLEN edition exluding splits'
+SELECT COUNT(*) AS 'Total number of letters in DB that are not part of the ALLEN edition excluding splits'
 FROM era_cdb_v3.letters
-WHERE letters.letter_no_in_edition IS NOT NULL
-  AND letters.letter_no_in_edition NOT LIKE '%[ALLEN]%'
-  AND letters_id NOT LIKE '%ck2'
+WHERE (letters.letter_no_in_edition NOT LIKE '%[ALLEN]%'
+       OR letters.letter_no_in_edition IS NULL)
+  AND letters_id NOT LIKE '%ck2%'
