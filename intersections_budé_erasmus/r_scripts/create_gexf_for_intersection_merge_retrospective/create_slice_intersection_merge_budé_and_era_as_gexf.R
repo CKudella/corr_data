21885cf079e8eecb0d9da0707a650c3bc5de90c1
Christoph Kudella <Christoph.Kudella@googlemail.com>
2026-07-02 14:51:31 +0200

    fixed addressed data frame

--- intersections_budé_erasmus/r_scripts/create_gexf_for_intersection_merge_retrospective/create_slice_intersection_merge_budé_and_era_as_gexf.R
+++ intersections_budé_erasmus/r_scripts/create_gexf_for_intersection_merge_retrospective/create_slice_intersection_merge_budé_and_era_as_gexf.R
@@ -16,26 +16,26 @@ allepp <- read.csv("merge_scripts/intersection_merge/intersection_merge_era_and_
 mutcorr <- read.csv("intersection_overview/id_and_names_of_mut_corr_era_budé.csv", fileEncoding = "UTF-8", na.strings = c("NULL"))
 
 # cut unnecessary label parts from label column
-mutcorr$Label <- gsub("\\b(\\W+COE+.*)", "", mutcorr$Label)
-mutcorr$Label <- gsub("^(\\W+E)", "E", mutcorr$Label)
-mutcorr$Label <- gsub("^\\[", "", mutcorr$Label)
-mutcorr$Label <- stringr::str_replace_all(mutcorr$Label, "\\b\\p{Lu}{2,}\\b", function(m) {
+allcorr$Label <- gsub("\\b(\\W+COE+.*)", "", allcorr$Label)
+allcorr$Label <- gsub("^(\\W+E)", "E", allcorr$Label)
+allcorr$Label <- gsub("^\\[", "", allcorr$Label)
+allcorr$Label <- stringr::str_replace_all(allcorr$Label, "\\b\\p{Lu}{2,}\\b", function(m) {
   ifelse(stringr::str_detect(m, "^[IVXLCDM]+$"), m, stringr::str_to_title(m))
 })
 
 # override the programmatically derived labels
-erasmus_index <- which(mutcorr$Id == "17c580aa-3ba7-4851-8f26-9b3a0ebeadbf")
-mutcorr$Label[erasmus_index] <- "Desiderius Erasmus"
-unnamed_person_index <- which(mutcorr$Id == "be1dcbc4-3987-472a-b4a0-c3305ead139f")
-mutcorr$Label[unnamed_person_index] <- "[?]"
-martinus_of_ep_76_person_index <- which(mutcorr$Id == "ecbf6eb8-e2e8-48f8-a46a-2e34c0cd604c")
-mutcorr$Label[martinus_of_ep_76_person_index] <- "Martinus [of Ep 76]"
-ludovicus_bruges_index <- which(mutcorr$Id == "fb407a51-6dfa-48d4-9eb1-5c6b8ed756a5")
-mutcorr$Label[ludovicus_bruges_index] <- "Ludovicus [documented at Bruges 1517-18]"
-ludovicus_of_ep_167_index <- which(mutcorr$Id == "d26ad69b-74c9-4213-97fe-e4bccc9bbe33")
-mutcorr$Label[ludovicus_of_ep_167_index] <- "Ludovicus [Ludovicus [of Ep 167]"
-cornelis_of_bergen_of_ep_1562_index <- which(mutcorr$Id == "de457db2-5ab6-4928-ad83-954d7c89876a")
-mutcorr$Label[cornelis_of_bergen_of_ep_1562_index] <- "Cornelis of Bergen [of Ep 1562]"
+erasmus_index <- which(allcorr$Id == "17c580aa-3ba7-4851-8f26-9b3a0ebeadbf")
+allcorr$Label[erasmus_index] <- "Desiderius Erasmus"
+unnamed_person_index <- which(allcorr$Id == "be1dcbc4-3987-472a-b4a0-c3305ead139f")
+allcorr$Label[unnamed_person_index] <- "[?]"
+martinus_of_ep_76_person_index <- which(allcorr$Id == "ecbf6eb8-e2e8-48f8-a46a-2e34c0cd604c")
+allcorr$Label[martinus_of_ep_76_person_index] <- "Martinus [of Ep 76]"
+ludovicus_bruges_index <- which(allcorr$Id == "fb407a51-6dfa-48d4-9eb1-5c6b8ed756a5")
+allcorr$Label[ludovicus_bruges_index] <- "Ludovicus [documented at Bruges 1517-18]"
+ludovicus_of_ep_167_index <- which(allcorr$Id == "d26ad69b-74c9-4213-97fe-e4bccc9bbe33")
+allcorr$Label[ludovicus_of_ep_167_index] <- "Ludovicus [Ludovicus [of Ep 167]"
+cornelis_of_bergen_of_ep_1562_index <- which(allcorr$Id == "de457db2-5ab6-4928-ad83-954d7c89876a")
+allcorr$Label[cornelis_of_bergen_of_ep_1562_index] <- "Cornelis of Bergen [of Ep 1562]"
 
 # add colour for all correspondents
 allcorr$colour <- "#525252"
