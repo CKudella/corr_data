7790fbd15c5a336863e4ed9d5f8bedc071d52d9a
Christoph Kudella <Christoph.Kudella@googlemail.com>
2026-08-05 16:14:45 +0200

    added query

---
+++ erasmus/sql_queries/duration_of_correspondence/duration_and_direction_corr_all_corr.sql
@@ -0,0 +1,35 @@
+SELECT C.correspondents_id,
+       C.name_in_edition,
+       LEAST(IFNULL(X.FLFE, Y.FLTE), IFNULL(Y.FLTE, X.FLFE)) AS 'Beginning of the correspondence',
+       GREATEST(IFNULL(X.LLFE, Y.LLTE), IFNULL(Y.LLTE, X.LLFE)) AS 'End of the correspondence',
+       X.FLFE AS 'First letter FROM Erasmus TO this correspondent',
+       X.LLFE AS 'Last letter FROM Erasmus TO this correspondent',
+       Y.FLTE AS 'First letter FROM this correspondent TO Erasmus',
+       Y.LLTE AS 'Last letter FROM this correspondent TO Erasmus',
+       CASE
+           WHEN X.FLFE IS NOT NULL THEN 1
+           ELSE 0
+       END AS 'Has letters FROM Erasmus',
+       CASE
+           WHEN Y.FLTE IS NOT NULL THEN 1
+           ELSE 0
+       END AS 'Has letters TO Erasmus'
+FROM
+  (SELECT DISTINCT CA.correspondents_id,
+                   CA.name_in_edition
+   FROM era_cdb.correspondents AS CA) AS C
+LEFT OUTER JOIN
+  (SELECT DISTINCT XA.recipient_id,
+                   MIN(XA.send_date_computable1) AS FLFE,
+                   MAX(XA.send_date_computable1) AS LLFE
+   FROM era_cdb.letters AS XA
+   WHERE XA.sender_id = '17c580aa-3ba7-4851-8f26-9b3a0ebeadbf'
+   GROUP BY XA.recipient_id) AS X ON C.correspondents_id = X.recipient_id
+LEFT OUTER JOIN
+  (SELECT DISTINCT YA.sender_id,
+                   MIN(YA.send_date_computable1) AS FLTE,
+                   MAX(YA.send_date_computable1) AS LLTE
+   FROM era_cdb.letters AS YA
+   WHERE YA.recipient_id = '17c580aa-3ba7-4851-8f26-9b3a0ebeadbf'
+   GROUP BY YA.sender_id) AS Y ON C.correspondents_id = Y.sender_id
+WHERE correspondents_id != '17c580aa-3ba7-4851-8f26-9b3a0ebeadbf'
